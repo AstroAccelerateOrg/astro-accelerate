b6d10508e8d74dc086940fcd56318189ded05bb8
Nassim Ouannoughi <nassim.ouannoughi@gmail.com>
2016-10-10 15:43:34 +0100

    remove bin + outputs

--- lib/host_fold.cu
+++ lib/host_fold.cu
@@ -4,7 +4,7 @@
 #include <math.h>
 #include "AstroAccelerate/params.h"
 
-void fold(int range, int nsamp, int max_ndms, int processed, int nboots, int num_trial_bins, int navdms, float narrow, float wide, int nsearch, float aggression, float cutoff, float ***output_buffer, int *ndms, int *inBin, float *dm_low, float *dm_high, float *dm_step, float tsamp) {
+void periodicity(int range, int nsamp, int max_ndms, int processed, int nboots, int num_trial_bins, int navdms, float narrow, float wide, int nsearch, float aggression, float cutoff, float ***output_buffer, int *ndms, int *inBin, float *dm_low, float *dm_high, float *dm_step, float tsamp) {
 
 	int nharms = 32;
 
