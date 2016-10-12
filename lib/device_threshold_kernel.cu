be5633612d866d47ca9bfcc2ef904df30f66d7f5
Nassim Ouannoughi <nassim.ouannoughi@gmail.com>
2016-10-12 11:21:38 +0100

    updated library
    Former-commit-id: fe80b9c735d1c898047cbb64bcf8da05cd6a21da

--- lib/device_threshold_kernel.cu
+++ lib/device_threshold_kernel.cu
@@ -7,8 +7,7 @@
 #include <cuda_runtime.h>
 #include "AstroAccelerate/params.h"
 
-__global__ void THR_GPU_WARP_ignore(float2 const* __restrict__ d_input, float2 const* __restrict__ d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nTaps, int nTimesamples, int max_list_size)
-{
+__global__ void THR_GPU_WARP_ignore(float2 const* __restrict__ d_input, unsigned char *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nTaps, int nTimesamples, int max_list_size) {
 	int local_id;
 	local_id = threadIdx.x & (WARP - 1);
 	int warp_id;
@@ -19,27 +18,27 @@ __global__ void THR_GPU_WARP_ignore(float2 const* __restrict__ d_input, float2 c
 	
 	pos = (blockIdx.y*THR_WARPS_PER_BLOCK + warp_id)*(nTimesamples>>1) + blockIdx.x*WARP*THR_ELEM_PER_THREAD + local_id;
 	
-	for(int i=0; i<THR_ELEM_PER_THREAD; i++)
-	{
-		width = d_input_taps[pos];
-		R = __ldg(&d_input[pos]);
-		if(R.x > threshold && width.x<nTaps)
-		{
+	for(int i=0; i<THR_ELEM_PER_THREAD; i++){
+		R=__ldg(&d_input[pos]);
+		width.x=(float) d_input_taps[2*pos];
+		width.y=(float) d_input_taps[2*pos+1];
+		
+		if(R.x > threshold && width.x<nTaps) {
+		//if(R.x > threshold) {
 			mask=__ballot(1);
 			leader=__ffs(mask)-1;
 			if(local_id==leader) list_pos=atomicAdd(gmem_pos,__popc(mask));
 			list_pos=__shfl(list_pos,leader);
 			list_pos=list_pos+__popc(mask&((1<<local_id)-1));
-			if(list_pos<max_list_size)
-			{
+			if(list_pos<max_list_size){
 				d_output_list[4*list_pos]   = blockIdx.y*THR_WARPS_PER_BLOCK + warp_id;
 				d_output_list[4*list_pos+1] = 2*((blockIdx.x*THR_ELEM_PER_THREAD + i)*WARP + local_id);
 				d_output_list[4*list_pos+2] = R.x;
 				d_output_list[4*list_pos+3] = width.x;
 			}
 		}
-		if(R.y > threshold && width.y<nTaps)
-		{
+		if(R.y > threshold && width.y<nTaps) {
+		//if(R.y > threshold) {
 			mask=__ballot(1);
 			leader=__ffs(mask)-1;
 			if(local_id==leader) list_pos=atomicAdd(gmem_pos,__popc(mask));
@@ -57,8 +56,7 @@ __global__ void THR_GPU_WARP_ignore(float2 const* __restrict__ d_input, float2 c
 }
 
 
-__global__ void THR_GPU_WARP(float2 const* __restrict__ d_input, float2 const* __restrict__ d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nTimesamples, int max_list_size)
-{
+__global__ void THR_GPU_WARP(float2 const* __restrict__ d_input, unsigned char *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nTimesamples, int max_list_size) {
 	int local_id;
 	local_id = threadIdx.x & (WARP - 1);
 	int warp_id;
@@ -69,37 +67,32 @@ __global__ void THR_GPU_WARP(float2 const* __restrict__ d_input, float2 const* _
 	
 	pos = (blockIdx.y*THR_WARPS_PER_BLOCK + warp_id)*(nTimesamples>>1) + blockIdx.x*WARP*THR_ELEM_PER_THREAD + local_id;
 	
-	for(int i=0; i<THR_ELEM_PER_THREAD; i++)
-	{
+	for(int i=0; i<THR_ELEM_PER_THREAD; i++){
 		R=__ldg(&d_input[pos]);
-		if(R.x > threshold)
-		{
+		if(R.x > threshold) {
 			mask=__ballot(1);
 			leader=__ffs(mask)-1;
 			if(local_id==leader) list_pos=atomicAdd(gmem_pos,__popc(mask));
 			list_pos=__shfl(list_pos,leader);
 			list_pos=list_pos+__popc(mask&((1<<local_id)-1));
-			if(list_pos<max_list_size)
-			{
+			if(list_pos<max_list_size){
 				d_output_list[4*list_pos]   = blockIdx.y*THR_WARPS_PER_BLOCK + warp_id;
 				d_output_list[4*list_pos+1] = 2*((blockIdx.x*THR_ELEM_PER_THREAD + i)*WARP + local_id);
 				d_output_list[4*list_pos+2] = R.x;
-				d_output_list[4*list_pos+3] = d_input_taps[pos].x;
+				d_output_list[4*list_pos+3] = d_input_taps[2*pos];
 			}
 		}
-		if(R.y > threshold)
-		{
+		if(R.y > threshold) {
 			mask=__ballot(1);
 			leader=__ffs(mask)-1;
 			if(local_id==leader) list_pos=atomicAdd(gmem_pos,__popc(mask));
 			list_pos=__shfl(list_pos,leader);
 			list_pos=list_pos+__popc(mask&((1<<local_id)-1));
-			if(list_pos<max_list_size)
-			{
+			if(list_pos<max_list_size){
 				d_output_list[4*list_pos]   = blockIdx.y*THR_WARPS_PER_BLOCK + warp_id;
 				d_output_list[4*list_pos+1] = 2*((blockIdx.x*THR_ELEM_PER_THREAD + i)*WARP + local_id)+1;
 				d_output_list[4*list_pos+2] = R.y;
-				d_output_list[4*list_pos+3] = d_input_taps[pos].y;
+				d_output_list[4*list_pos+3] = d_input_taps[2*pos+1];
 			}
 		}
 		pos = pos + WARP;
