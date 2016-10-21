af0f24f7d469ab3d52695ce601b1f515ebda73e5
Wes Armour <wes.armour@oerc.ox.ac.uk>
2016-10-21 09:09:59 +0100

    Added a simple zero DM GPU kernel. The code needs a keyword adding in host_get_user_input so that this feature can be turned on and off
    Former-commit-id: ab08c9175c81460abcb03fc652a6f34dbc440b1b

---
+++ lib/AstroAccelerate/device_zero_dm.h
@@ -0,0 +1,7 @@
+#ifndef __ZERODM__
+#define __ZERODM__
+
+extern void zero_dm(unsigned short *d_input, int nchans, int nsamp);
+
+#endif
+
---
+++ lib/device_zero_dm_kernel.cu
@@ -0,0 +1,24 @@
+#ifndef ZERODM_KERNEL_H_
+#define ZERODM_KERNEL_H_
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include "AstroAccelerate/params.h"
+
+
+//{{{ zero dm kernel - needs cleaning and optimizing // WA 21/10/16
+__global__ void zero_dm_kernel(unsigned short *d_input, int nchans, int nsamp)
+{
+
+	int t  = blockIdx.x * blockDim.x + threadIdx.x;
+
+	float sum = 0.0f;
+	for(int c = 0; c < nchans; c++) sum+=(float)__ldg(&d_input[t*nchans + c]);
+	sum = (sum/(float)nchans);
+	for(int c = 0; c < nchans; c++) d_input[t*nchans + c]=(unsigned short)((unsigned char)((float)d_input[t*nchans + c]-sum));
+}
+
+//}}}
+
+#endif
+
