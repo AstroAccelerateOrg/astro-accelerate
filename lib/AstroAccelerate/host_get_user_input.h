9730f59cb3061c03f9497d46e1601464a80e02e1
Nassim Ouannoughi <nassim.ouannoughi@gmail.com>
2016-10-31 16:33:02 +0000

    add zero_dm parameter

--- lib/AstroAccelerate/host_get_user_input.h
+++ lib/AstroAccelerate/host_get_user_input.h
@@ -1,5 +1,5 @@
 #ifndef __GETUSERINPUT__
 #define __GETUSERINPUT__
 
-void get_user_input(FILE **fp, int argc, char *argv[], int *multi_file, int *enable_debug, int *enable_analysis, int *enable_periodicity, int *enable_acceleration, int *output_dmt, int *nboots, int *ntrial_bins, int *navdms, float *narrow, float *wide, float *aggression, int *nsearch, int **inBin, int **outBin, float *power, float *sigma_cutoff, int *range, float **user_dm_low, float **user_dm_high, float **user_dm_step);
+void get_user_input(FILE **fp, int argc, char *argv[], int *multi_file, int *enable_debug, int *enable_analysis, int *enable_periodicity, int *enable_acceleration, int *output_dmt, int *enable_zero_dm, int *nboots, int *ntrial_bins, int *navdms, float *narrow, float *wide, float *aggression, int *nsearch, int **inBin, int **outBin, float *power, float *sigma_cutoff, int *range, float **user_dm_low, float **user_dm_high, float **user_dm_step);
 #endif
