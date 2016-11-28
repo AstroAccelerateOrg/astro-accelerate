922bf57085ddcf36e4fa135cb6498ee38155dddc
KAdamek <karel.adamek@oerc.ox.ac.uk>
2016-11-28 13:50:38 +0000

    Separated CPU and GPU SPS codes and fixed some bugs
    device_analysis.cu contains GPU SPS code. CPU code is in
    host_analysis.cu. There is no switch for CPU code. fixed a bug which
    produced garbage data in SPS code. This was due to thresholding kernel
    which processed more data then it should.

--- lib/AstroAccelerate/device_threshold.h
+++ lib/AstroAccelerate/device_threshold.h
@@ -2,8 +2,8 @@
 #define __THRESHOLD__
 
 extern void THR_init(void);
-extern int THRESHOLD(float *d_input, unsigned char *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nDMs, int nTimesamples, int max_list_size);
-extern int THRESHOLD_ignore(float *d_input, unsigned char *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nTaps, int nDMs, int nTimesamples, int max_list_size);
+extern int THRESHOLD(float *d_input, unsigned char *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nDMs, int nTimesamples, int offset, int max_list_size);
+extern int THRESHOLD_ignore(float *d_input, unsigned char *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nTaps, int nDMs, int nTimesamples, int offset, int max_list_size);
 
 #endif
 
