82a7e6d0b62f160733f308757a1836dc3849e2dd
Wes Armour <wes.armour@oerc.ox.ac.uk>
2016-11-30 14:28:32 +0000

    Adding profiling script to determine optimal paramters for the params.h file

---
+++ scripts/profiling.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+maxThreads=1024
+
+rm -rf profile_results
+
+mkdir profile_results
+
+for unroll in {16,32,64}
+do
+	for acc in {6,8,10}
+	do
+		for divint in {6,8,10}
+		do
+			for divindm in {32,40}
+			do
+				threads=$(echo "$divint * $divindm" | bc)
+				if [ $threads -le $maxThreads ]; then
+					cd ../lib
+					pwd
+					rm AstroAccelerate/params.h
+					echo "#define ACCMAX 350" > ./params.txt
+					echo "#define ACCSTEP 11" >> ./params.txt
+					echo "#define UNROLLS $unroll" >> ./params.txt
+					echo "#define SNUMREG $acc" >> ./params.txt
+					echo "#define SDIVINT $divint" >> ./params.txt
+					echo "#define SDIVINDM $divindm" >> ./params.txt
+					echo "#define SFDIVINDM $divindm.0f" >> ./params.txt
+					echo "#define CARD 1" >> ./params.txt
+					echo "#define NOPSSHIFT 5" >> ./params.txt
+					echo "#define NOPSLOOP 3" >> ./params.txt
+					echo "#define NDATAPERLOOP 1" >> ./params.txt
+					echo "#define BINDIVINT 6" >> ./params.txt
+					echo "#define BINDIVINF 32" >> ./params.txt
+					echo "#define CT 256" >> ./params.txt
+					echo "#define CF 2" >> ./params.txt
+					echo "#define NOPS 4.0" >> ./params.txt
+					echo "#define STATST 128" >> ./params.txt
+					echo "#define STATSLOOP 8" >> ./params.txt
+
+					mv params.txt AstroAccelerate/params.h
+	
+					make clean
+			
+					regcount=$(make -j 16 2>&1 | grep -A2 shared_dedisperse_kernel | tail -1 | awk -F" " '{print $5}')
+
+					cd ../scripts/
+
+					./astro-accelerate.sh ../input_files/ska_tune.txt > profile_results/u"$unroll"_a"$acc"_t"$divint"_dm"$divindm"_r"$regcount".dat
+			
+					echo "unrolls: $unroll	acc: $acc    divint: $divint    divindm: $divindm    reg: $regcount"
+			
+				fi
+			done
+		done
+	done
+done
