8d08f5a249ce6ab133d07f1e04c9e91b745df0b8
James Sharpe <james.sharpe@zenotech.com>
2017-02-20 13:19:46 +0000

    Add code for outputting power utilisation

--- lib/host_peak_find_perf_main.cpp
+++ lib/host_peak_find_perf_main.cpp
@@ -6,17 +6,84 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <condition_variable>
 
+#include <unistd.h>
 #include <omp.h>
+
+#include "nvml.h"
+
+class PowerMonitor {
+public:
+    PowerMonitor() : active(true), pm(&PowerMonitor::monitor_power, this) {
+        sleep(1);
+    }
+
+    ~PowerMonitor() {
+        active = false;
+        m_end = true;
+        start_cond.notify_all();
+
+        pm.join();
+    }
+
+    void wait_for_start() {
+        std::unique_lock<std::mutex> lock(m);
+        m_end = false;
+        start_cond.wait(lock);
+    }
+
+    void start() {
+        start_cond.notify_all();
+    }
+
+    void end() {
+        m_end = true; 
+    }
+
+    void csv_report() {
+        std::cout << stats.N << ", " << stats.mean() << ", " << stats.variance();
+    }
+
+private:
+    void monitor_power() {
+         nvmlDevice_t device;
+         unsigned int power;
+         nvmlDeviceGetHandleByIndex(0, &device);
+         while (active) {
+             wait_for_start();
+             m.unlock();
+             do {
+                 nvmlDeviceGetPowerUsage(device, &power);
+                 stats(power);
+                 usleep(1);
+             } while (!m_end);
+         }
+    }
+
+private:
+    volatile bool active; //Has to be above the thread so its initialised first
+    std::thread pm;
+
+    std::mutex m;
+    std::condition_variable start_cond;
+    volatile bool m_end;
+    accumulator<unsigned int, unsigned long long> stats;
+
+};
 
 template<typename Func>
 void test_algorithm(const std::vector<float, managed_allocator<float>> & data, std::vector<unsigned short, managed_allocator<unsigned short>> output, int width, int height, Func peakfind, int ntrials, std::string algoname) {
+    PowerMonitor power_monitor;
     accumulator<float, double> stats;
     double start_t, end_t;
     for (int i=0; i < ntrials; ++i) {
+            power_monitor.start();
 	    start_t = omp_get_wtime();
 	    peakfind(data.data(), 0, width, height, output.data(), 0.0f);
 	    end_t = omp_get_wtime();
+            power_monitor.end();
 	    stats(end_t-start_t);
     }
     cudaDeviceSynchronize();
@@ -27,18 +94,21 @@ void test_algorithm(const std::vector<float, managed_allocator<float>> & data, s
 	    }
     }
     auto time = stats.sum;
-    auto data_mb = ntrials*data.size() * sizeof(float) / (1024 * 1024);
-    std::cout << algoname << ", " << data.size() * sizeof(float) / (1024*1024) << ", " << data_mb/(1024 * time) << ", " << stats.mean() << ", " << stats.variance() << ", " << peak_count << std::endl;
+    auto data_gb = ntrials*data.size() * sizeof(float) / (1024 * 1024 * 1024);
+    std::cout << algoname << ", " << data.size() * sizeof(float) / (1024*1024) << ", " << data_gb/time << ", " << stats.mean() << ", " << stats.variance() << ", " << peak_count << ", ";
+    power_monitor.csv_report();
+    std::cout << std::endl;
 }
 
 int main(int argc, char * argv[]) {
+    nvmlInit();
     std::vector<float, managed_allocator<float>> data;
     int ntrials = 10;
     if (argc > 1) {
         ntrials = atoi(argv[1]);
     }
     std::cerr << "Running with " << ntrials << " trials\n";
-    std::cout << "Algorithm, DataSize, data_rate(GB/s), mean time, variance, peaks\n";
+    std::cout << "Algorithm, DataSize, data_rate(GB/s), mean time, variance, peaks, power samples, mean power (mW), power variance\n";
 
     while (true) {
 	    data.resize(data.size() + 128ul*1024*1024/sizeof(float));
