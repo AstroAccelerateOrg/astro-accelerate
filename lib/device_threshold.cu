de67f1573ff2c9fae39cc28d4f61fb0679706701
KAdamek <karel.adamek@oerc.ox.ac.uk>
2017-02-20 18:00:35 +0000

    Added new boxcar filters
    Added new boxcar filters, also modified tresholding.
    
    
    Former-commit-id: c74738605e645002cd9aa62023e1f209f36ca306

--- lib/device_threshold.cu
+++ lib/device_threshold.cu
@@ -3,39 +3,45 @@
 #include "AstroAccelerate/params.h"
 #include "device_threshold_kernel.cu"
 
-void THR_init(void)
-{
+void THR_init(void) {
 	//---------> Specific nVidia stuff
 	cudaDeviceSetCacheConfig (cudaFuncCachePreferShared);
 	cudaDeviceSetSharedMemConfig (cudaSharedMemBankSizeFourByte);
 }
 
-int THRESHOLD(float *d_input, unsigned char *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nDMs, int nTimesamples, int offset, int max_list_size)
-{
+int THRESHOLD(float *d_input, ushort *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nDMs, int nTimesamples, int offset, int max_iteration, int max_list_size) {
 	//---------> Task specific
-	int nBlocks, nRest, Elements_per_block;
-
-	Elements_per_block = 2*WARP*THR_ELEM_PER_THREAD;
-	nBlocks = (nTimesamples-offset)/Elements_per_block;
-	nRest = (nTimesamples-offset) - nBlocks*Elements_per_block;
-	if(nRest>0) nBlocks++;
-
-	//---------> CUDA block and CUDA grid parameters
-	int nCUDAblocks_x = nBlocks;
-	int nCUDAblocks_y = nDMs/THR_WARPS_PER_BLOCK;
-
-	dim3 gridSize(nCUDAblocks_x, nCUDAblocks_y, 1);
+	int nBlocks, nRest, Elements_per_block, output_offset, decimated_timesamples;
+	int nCUDAblocks_x, nCUDAblocks_y;
+	
+	dim3 gridSize(1, 1, 1);
 	dim3 blockSize(WARP*THR_WARPS_PER_BLOCK, 1, 1);
-
-	//---------> Pulse detection FIR
+	
 	THR_init();
-	THR_GPU_WARP<<<gridSize, blockSize>>>((float2 *) d_input, d_input_taps, d_output_list, gmem_pos, threshold, nTimesamples, nTimesamples-offset, max_list_size);
+	
+	output_offset=0;
+	for(int f=0; f<max_iteration; f++){
+		decimated_timesamples = (nTimesamples>>f);
+		
+		Elements_per_block = 2*WARP*THR_ELEM_PER_THREAD;
+		nBlocks = (decimated_timesamples-offset)/Elements_per_block;
+		nRest = (decimated_timesamples-offset) - nBlocks*Elements_per_block;
+		if(nRest>0) nBlocks++;
+		
+		nCUDAblocks_x = nBlocks;
+		nCUDAblocks_y = nDMs/THR_WARPS_PER_BLOCK;
+		
+		gridSize.x=nCUDAblocks_x; gridSize.y=nCUDAblocks_y; gridSize.z=1;
+		blockSize.x=WARP*THR_WARPS_PER_BLOCK; blockSize.y=1; blockSize.z=1;
+		
+		THR_GPU_WARP<<<gridSize, blockSize>>>((float2 *) &d_input[output_offset], &d_input_taps[output_offset], d_output_list, gmem_pos, threshold, decimated_timesamples, decimated_timesamples-offset, max_list_size);
+		output_offset = output_offset + nDMs*decimated_timesamples;
+	}
 
-	return ( 0 );
+	return (0);
 }
 
-int THRESHOLD_ignore(float *d_input, unsigned char *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nTaps, int nDMs, int nTimesamples, int offset, int max_list_size)
-{
+int THRESHOLD_ignore(float *d_input, unsigned char *d_input_taps, float *d_output_list, int *gmem_pos, float threshold, int nTaps, int nDMs, int nTimesamples, int offset, int max_list_size) {
 	//---------> Task specific
 	int nBlocks, nRest, Elements_per_block;
 
@@ -54,5 +60,5 @@ int THRESHOLD_ignore(float *d_input, unsigned char *d_input_taps, float *d_outpu
 	THR_init();
 	THR_GPU_WARP_ignore<<<gridSize, blockSize>>>((float2 *) d_input, d_input_taps, d_output_list, gmem_pos, threshold, nTaps, nTimesamples, max_list_size);
 
-	return ( 0 );
+	return (0);
 }
