17fef80c149f63d98aa88794c33a83dc5993387b
ania.brown <ania.brown@gmail.com>
2017-02-22 11:33:23 +0000

    cleanup. changed script names and moved all scripts to script folder
    Former-commit-id: 24e1cd0bf832c12a312de94d444c7209d787ba51

---
+++ scripts/generateDedispersionKernelCode.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+maxRange=${1-7}
+
+kernelFilename=../lib/device_dedispersion_kernel.cu
+kernelTemplatePrefix=../lib/device_dedispersion_kernel
+rm $kernelFilename
+
+cat "$kernelTemplatePrefix"_header_template.cu > $kernelFilename
+
+for range in `seq 0 $maxRange`
+do
+	cat "$kernelTemplatePrefix"_template.cu | sed "s/{RANGE}/_$range/g" >> $kernelFilename
+done
+
+cat "$kernelTemplatePrefix"_footer_template.cu >> $kernelFilename
+
+
+kernelFunctionsFilename=../lib/AstroAccelerate/kernel_functions.h
+kernelFunctionsTemplatePrefix=../lib/AstroAccelerate/kernel_functions
+rm $kernelFunctionsFilename
+
+for range in `seq 0 $maxRange`
+do
+	unrolls+=UNROLLS_$range
+	snumreg+=SNUMREG_$range
+	sdivint+=SDIVINT_$range
+	sdivindm+=SDIVINDM_$range
+	kernelFunctions+=shared_dedisperse_kernel_range_$range
+	if [ $range -lt $maxRange ]
+	then
+		unrolls+=,
+		snumreg+=,
+		sdivint+=,
+		sdivindm+=,
+		kernelFunctions+=,
+	fi
+done
+
+cat "$kernelFunctionsTemplatePrefix"_template.h | sed "s/{UNROLLS_RANGES}/$unrolls/g" | sed "s/{SNUMREG_RANGES}/$snumreg/g" | sed "s/{SDIVINT_RANGES}/$sdivint/g" | sed "s/{SDIVINDM_RANGES}/$sdivindm/g" | sed "s/{KERNEL_FUNCTION_RANGES}/$kernelFunctions/g" >> $kernelFunctionsFilename
