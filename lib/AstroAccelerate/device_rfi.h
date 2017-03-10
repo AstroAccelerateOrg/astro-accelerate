dae941f9a2cb2e03d5012febd6c6f4efc47ae3cb
NassimOuannoughi <nassim.ouannoughi@gmail.com>
2017-03-10 11:15:25 +0000

    remove temporary folder

--- lib/AstroAccelerate/device_rfi.h
+++
@@ -1,7 +0,0 @@
-#ifndef ASTROACCELERATE_RFIGPU_H_
-#define ASTROACCELERATE_RFIGPU_H_
-
-extern void rfi_gpu(unsigned short *d_input, int nchans, int nsamp);
-
-#endif
-
--- lib/AstroAccelerate/device_zero_dm.h
+++
@@ -1,7 +0,0 @@
-#ifndef ASTROACCELERATE_ZERODM_H_
-#define ASTROACCELERATE_ZERODM_H_
-
-extern void zero_dm(unsigned short *d_input, int nchans, int nsamp);
-
-#endif
-
