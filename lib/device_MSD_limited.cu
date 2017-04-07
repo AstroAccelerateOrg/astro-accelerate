df1cf07fd6b1b4e7ab5eccddbc65890153c2f727
KAdamek <karel.adamek@oerc.ox.ac.uk>
2017-04-07 16:00:47 +0100

    New MSD kernel for linear approxiamtion of standard deviation

--- lib/device_MSD_limited.cu
+++ lib/device_MSD_limited.cu
@@ -3,29 +3,24 @@
 #include "headers/params.h"
 #include "device_MSD_limited_kernel.cu"
 
-int Choose_x_dim(int grid_dim)
-{
-	int seive[15] =
-	{ 32, 31, 29, 23, 19, 17, 16, 13, 11, 8, 7, 5, 4, 3, 2 };
+int Choose_x_dim(int grid_dim){
+	int seive[15] =	{ 32, 31, 29, 23, 19, 17, 16, 13, 11, 8, 7, 5, 4, 3, 2 };
 
 	int f, nRest, nBlocks, N, N_accepted;
 
 	N = 1;
 	N_accepted = 1;
-	for (int i = 0; i < 4; i++)
-	{
-		for (f = 0; f < 15; f++)
-		{
+	for (int i = 0; i < 4; i++)	{
+		for (f = 0; f < 15; f++) {
 			nBlocks = grid_dim / seive[f];
-			nRest = grid_dim - nBlocks * seive[f];
-			if (nRest == 0)
-			{
-				N_accepted = N_accepted * N;
+			nRest = grid_dim - nBlocks*seive[f];
+			if (nRest == 0) {
+				N_accepted = N_accepted*N;
 				N = seive[f];
 				break;
 			}
 		}
-		if (( N_accepted * N ) > 32 || N == 1)
+		if (( N_accepted*N ) > 32 || N == 1)
 			return ( N_accepted );
 		grid_dim = grid_dim / N;
 	}
@@ -33,20 +28,16 @@ int Choose_x_dim(int grid_dim)
 	return ( N_accepted );
 }
 
-int Choose_y_dim(int grid_dim)
-{
-	int seive[5] =
-	{ 32, 16, 8, 4, 2 };
+int Choose_y_dim(int grid_dim){
+	int seive[5] = { 32, 16, 8, 4, 2 };
 
 	int f, nRest, nBlocks, N;
 
 	N = 1;
-	for (f = 0; f < 5; f++)
-	{
+	for (f = 0; f < 5; f++) {
 		nBlocks = grid_dim / seive[f];
-		nRest = grid_dim - nBlocks * seive[f];
-		if (nRest == 0)
-		{
+		nRest = grid_dim - nBlocks*seive[f];
+		if (nRest == 0) {
 			N = seive[f];
 			break;
 		}
@@ -55,19 +46,16 @@ int Choose_y_dim(int grid_dim)
 	return ( N );
 }
 
-void MSD_limited_init()
-{
+void MSD_limited_init() {
 	//---------> Specific nVidia stuff
 	cudaDeviceSetCacheConfig (cudaFuncCachePreferShared);
 	cudaDeviceSetSharedMemConfig (cudaSharedMemBankSizeFourByte);
 }
 
-int MSD_limited(float *d_input, float *d_MSD, int nDMs, int nTimesamples, int offset)
-{
+
+int MSD_limited(float *d_input, float *d_MSD, int nDMs, int nTimesamples, int offset) {
 	//---------> Task specific
-	int nBlocks_x, nBlocks_y, nBlocks_total, nSteps_x, nSteps_y, nRest, nThreads,
-	    epw; //epw = elements per warp 32 for float 64 for float2
-	// int nElements;
+	int nBlocks_x, nBlocks_y, nBlocks_total, nSteps_x, nSteps_y, nRest, nThreads, epw; //epw = elements per warp 32 for float 64 for float2
 	float *d_output;
 
 	//---------> CUDA block and CUDA grid parameters
@@ -75,35 +63,34 @@ int MSD_limited(float *d_input, float *d_MSD, int nDMs, int nTimesamples, int of
 	epw = 32;
 	nBlocks_x = 0;
 	nRest = 0;
-	// nTimesamples must be divisible by 64 because otherwise it is extremely tedious and I refuse to do it.
+	
 	nSteps_x = Choose_x_dim(( nTimesamples ) / epw);
-	nBlocks_x = nBlocks_x + ( nTimesamples - offset ) / ( nSteps_x * epw );
-	nRest += nTimesamples - offset - nBlocks_x * nSteps_x * epw;
-	if (nRest > epw)
-		nBlocks_x++; // if nRest<64 then it means a lot of branching in the kernel and error it induces would be generally small.
+	nBlocks_x = nBlocks_x + ( nTimesamples - offset ) / ( nSteps_x*epw );
+	nRest += nTimesamples - offset - nBlocks_x*nSteps_x*epw;
+	if (nRest > epw) nBlocks_x++; // if nRest<64(32) then it means a lot of branching in the kernel and error it induces would be generally small.
 
 	nSteps_y = Choose_y_dim(nDMs);
 	nBlocks_y = nDMs / nSteps_y;
 	// I do not calculate nRest here since I assume it will be always divisible by nSteps_y.
 
-	nBlocks_total = nBlocks_x * nBlocks_y;
-	//nElements = nBlocks_total * nSteps_x * epw * nSteps_y;
+	nBlocks_total = nBlocks_x*nBlocks_y;
+	//nElements = nBlocks_total*nSteps_x*epw*nSteps_y;
 
-	nThreads = nSteps_y * WARP;
+	nThreads = nSteps_y*WARP;
 
 	// calculation of the partials
 	dim3 gridSize(nBlocks_x, nBlocks_y, 1);
 	dim3 blockSize(nThreads, 1, 1);
 
 	dim3 final_gridSize(1, 1, 1);
-	dim3 final_blockSize(WARP * 4, 1, 1);
+	dim3 final_blockSize(WARP*4, 1, 1);
 
 	//---------> Allocation of temporary memory
-	cudaMalloc((void **) &d_output, nBlocks_total * 3 * sizeof(float));
+	cudaMalloc((void **) &d_output, nBlocks_total*3*sizeof(float));
 
 	//---------> MSD
 	MSD_init();
-	MSD_GPU_limited<<<gridSize, blockSize, nThreads * 12>>>(d_input, d_output, nSteps_x, nTimesamples, offset);
+	MSD_GPU_limited<<<gridSize, blockSize, nThreads*12>>>(d_input, d_output, nSteps_x, nTimesamples, offset);
 	MSD_GPU_limited_final<<<final_gridSize, final_blockSize>>>(d_output, d_MSD, nBlocks_total);
 
 	//---------> De-allocation of temporary memory
@@ -113,5 +100,65 @@ int MSD_limited(float *d_input, float *d_MSD, int nDMs, int nTimesamples, int of
 		return ( nRest );
 	else
 		return ( 0 );
+}
+
+int MSD_linear_approximation(float *d_input, float *d_MSD_T, int nTaps, int nDMs, int nTimesamples, int offset){
+	//---------> Task specific
+	int nBlocks_x, nBlocks_y, nBlocks_total, nSteps_x, nSteps_y, nRest; //epw = elements per warp 32 for float 64 for float2
+	float *d_output;
+	float *d_output_taps;
+	float *d_MSD_T_base;
+
+	//---------> CUDA block and CUDA grid parameters
+	// Determining in x direction (direction of data alignment)
+	printf("\n\n");
+	nBlocks_x=0; nRest=0;
+
+	nSteps_x=2*PD_NTHREADS-nTaps;
+	nBlocks_x=nBlocks_x + (nTimesamples-offset)/(nSteps_x);
+	nRest=nTimesamples - offset - nBlocks_x*nSteps_x;
+	if(nRest>0) nBlocks_x++; // if nRest<64 then it means a lot of branching in the kernel and error it induces would be generally small.
+	//printf("nSteps_x:%d;b nBlocks_x:%d; nRest:%d; \n", nSteps_x, nBlocks_x, nRest);
+	
+	nSteps_y=Choose_y_dim(nDMs);
+	nBlocks_y=nDMs/nSteps_y;
+	// I do not calculate nRest here since I assume it will be always divisible by nSteps_y.
+	//printf("nSteps_y:%d; nBlocks_y:%d; nRest:%d; \n", nSteps_y, nBlocks_y, 0);
+	
+	nBlocks_total=nBlocks_x*nBlocks_y;
+	
+	// calculation of the partials
+	dim3 gridSize(nBlocks_x, nBlocks_y, 1);
+	dim3 blockSize(PD_NTHREADS, 1, 1);
+	
+	dim3 final_gridSize(1, 1, 1);
+	dim3 final_blockSize(WARP*4, 1, 1);
+	
+	// ----------------------------------------------->
+	// --------> Measured part (Pulse detection FIR)
+	
+	//---------> Allocation of temporary memory
+	cudaMalloc((void **) &d_output, nBlocks_total*3*sizeof(float));
+	cudaMalloc((void **) &d_output_taps, nBlocks_total*3*sizeof(float));
+	cudaMalloc((void **) &d_MSD_T_base, sizeof(float)*3);
+	
+	
+	//---------> MSD
+	MSD_init();
+	MSD_GPU_LA_ALL<<<gridSize,blockSize>>>(d_input, d_output, d_output_taps, nSteps_y, nTaps, nTimesamples, offset);
+	MSD_GPU_limited_final<<<final_gridSize, final_blockSize>>>(d_output, d_MSD_T_base, nBlocks_total);
+	MSD_GPU_limited_final_create_linear_approx<<<final_gridSize, final_blockSize>>>(d_output_taps, d_MSD_T, d_MSD_T_base, nTaps, nBlocks_total);
+		
+	//---------> De-allocation of temporary memory
+	cudaFree(d_output);
+	cudaFree(d_output_taps);
+	cudaFree(d_MSD_T_base);
+	
+	// --------> Measured part (Pulse detection FIR)
+	// ----------------------------------------------->
+	
+	if(nRest<64) return(nRest);
+	else return(0);
+	
 }
 
