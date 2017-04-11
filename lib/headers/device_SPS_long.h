5255510beb553716436a49496a5a344cf2319635
KAdamek <karel.adamek@oerc.ox.ac.uk>
2017-04-11 17:21:56 +0100

    Added SPS memory requirements into stratagy, reduced amount of output to console
    Added SPS memory requirements to stratagy - stratagy can now work with
    all available memory on GPU
    reduced amount of output to console - I've hidden it behind #defined
    flag
    1bit data fails

--- lib/headers/device_SPS_long.h
+++ lib/headers/device_SPS_long.h
@@ -1,6 +1,7 @@
 #ifndef __SPS_LONG__
 #define __SPS_LONG__
 
+size_t Get_memory_requirement_of_SPS();
 extern void PD_SEARCH_LONG_init();
 extern int PD_SEARCH_LONG(float *d_input, float *d_boxcar_values, float *d_decimated, float *d_output_SNR, int *d_output_taps, float *d_MSD, int max_boxcar_width, int nDMs, int nTimesamples, int *t_max_iterarion);
 extern int PD_SEARCH_LONG_BLN(float *d_input, float *d_boxcar_values, float *d_decimated, float *d_output_SNR, ushort *d_output_taps, float *d_MSD, int max_boxcar_width, int nDMs, int nTimesamples, int *t_max_iterarion);
--- lib/headers/host_stratagy.h
+++ lib/headers/host_stratagy.h
@@ -1,6 +1,6 @@
 #ifndef ASTROACCELERATE_STRATAGY_H_
 #define ASTROACCELERATE_STRATAGY_H_
 
-void stratagy(int *maxshift, int *max_samps, int *num_tchunks, int *max_ndms, int *total_ndms, float *max_dm, float power, int nchans, int nsamp, float fch1, float foff, float tsamp, int range, float *user_dm_low, float *user_dm_high, float *user_dm_step, float **dm_low, float **dm_high, float **dm_step, int **ndms, float **dmshifts, int *inBin, int ***t_processed, size_t *gpu_memory);
+void stratagy(int *maxshift, int *max_samps, int *num_tchunks, int *max_ndms, int *total_ndms, float *max_dm, float power, int nchans, int nsamp, float fch1, float foff, float tsamp, int range, float *user_dm_low, float *user_dm_high, float *user_dm_step, float **dm_low, float **dm_high, float **dm_step, int **ndms, float **dmshifts, int *inBin, int ***t_processed, size_t *gpu_memory, size_t SPS_mem_requirement);
 
 #endif
