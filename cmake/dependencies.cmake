f188d78e06af968a83e423675e4cc9b073104605
Ewan Barr <ewan.d.barr@googlmail.com>
2017-04-28 12:23:53 +0200

    Removed boost dependencies from cmake build files

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -20,14 +20,6 @@
 include(cmake/googletest.cmake)
 
 # other dependencies
-set(Boost_ADDITIONAL_VERSIONS "1.50 1.50.0")
-find_package(Boost COMPONENTS filesystem system program_options REQUIRED)
-
-include_directories(SYSTEM ${BOOST_INCLUDE_DIR})
-set(DEPENDENCY_LIBRARIES
-#    ${Boost_LIBRARIES}
-    )
-
 include(compiler_settings)
 include(cmake/cuda.cmake)
 include(cmake/doxygen.cmake)
