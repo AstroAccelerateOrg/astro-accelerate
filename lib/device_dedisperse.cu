4d150229fc1645819f1b16ebc140a332bb7c847b
Wes Armour <wes.armour@oerc.ox.ac.uk>
2017-05-08 01:54:02 +0100

    New 16 bit version of AA
    Former-commit-id: 9f5ba1347f3bc77c5964859cd96bc6ca3537225e

--- lib/device_dedisperse.cu
+++ lib/device_dedisperse.cu
@@ -4,66 +4,92 @@
 
 //{{{ dedisperse 
 
-void dedisperse(int i, int t_processed, int *inBin, float *dmshifts, unsigned short *d_input, float *d_output, int nchans, int nsamp, int maxshift, float *tsamp, float *dm_low, float *dm_high, float *dm_step, int *ndms)
+void dedisperse(int i, int t_processed, int *inBin, float *dmshifts, unsigned short *d_input, float *d_output, int nchans, int nsamp, int maxshift, float *tsamp, float *dm_low, float *dm_high, float *dm_step, int *ndms, int nbits)
 {
 
-	// FOR KEPLER SMEM....
-	float shift_one = ( SDIVINDM - 1 ) * ( dm_step[i] / ( *tsamp ) );
-	int shifta = (int) floorf(shift_one * dmshifts[nchans - 1]) + ( SDIVINT - 1 ) * 2;
-	int lineshift = shifta + ( ( SNUMREG - 1 ) * 2 * SDIVINT );
-	printf("\n%f", dm_step[i] / ( *tsamp ));
-	printf("\n%f", dmshifts[nchans - 1]);
-	printf("\n%d", shifta);
-	printf("\nlineshift:\t%d", lineshift);
+	if(nbits == 16) {
 
-	// Check to see if the threadblock will load a shared memory line that
-	// is long enough for the algorithm to run without an out of bounds
-	// access...
-	if (( ( SDIVINT - 1 ) + ( ( SDIVINDM - 1 ) * SDIVINT ) - 1 ) > lineshift)
-	{
+		float shift_one = ( SDIVINDM - 1 ) * ( dm_step[i] / ( *tsamp ) );
+		int shifta = (int) floorf(shift_one * dmshifts[nchans - 1]) + ( SDIVINT - 1 ) * 2;
+		int lineshift = shifta + ( ( SNUMREG - 1 ) * 2 * SDIVINT );
+		printf("\n%f", dm_step[i] / ( *tsamp ));
+		printf("\n%f", dmshifts[nchans - 1]);
+		printf("\n%d", shifta);
+		printf("\nlineshift:\t%d", lineshift);
 
-		/* FOR FERMI SMEM....
-		 float shift_one = (SDIVINDM-1)*(dm_step[i]/(*tsamp));
-		 int shift = (int)floorf(shift_one*dmshifts[nchans-1]);
-		 int lineshift = shift+((SNUMREG-1)*SDIVINT);
-		 //printf("\n%f",dm_step[i]/(*tsamp));
-		 //printf("\n%f",dmshifts[nchans-1]);
-		 //printf("\n%d",shift);
-		 //printf("\nlineshift:\t%d", lineshift);
-		 */
-		printf("\nUsing fast shared memory kernel");
+		// Check to see if the threadblock will load a shared memory line that
+		// is long enough for the algorithm to run without an out of bounds
+		// access...
+		if (( ( SDIVINT - 1 ) + ( ( SDIVINDM - 1 ) * SDIVINT ) - 1 ) > lineshift)
+		{
 
-		//{{{ Dedisperse data on the GPU 
-		float startdm = dm_low[i];
+			printf("\nUsing fast shared memory kernel");
 
-		int divisions_in_t = SDIVINT;
-		int divisions_in_dm = SDIVINDM;
-		int num_blocks_t = t_processed / ( divisions_in_t * 2 * SNUMREG );
-		int num_blocks_dm = ndms[i] / divisions_in_dm;
+			//{{{ Dedisperse data on the GPU 
+			float startdm = dm_low[i];
 
-//		printf("\ntpro:\t%d, numb:\t%d", t_processed, num_blocks_t), fflush(stdout);
+			int divisions_in_t = SDIVINT;
+			int divisions_in_dm = SDIVINDM;
+			int num_blocks_t = t_processed / ( divisions_in_t * 2 * SNUMREG );
+			int num_blocks_dm = ndms[i] / divisions_in_dm;
 
-//		if(ndms[i]%divisions_in_dm !=0) {
-//			printf("\nERROR: dm block size is not a divisor of the dm range!!");
-//			exit(0);
-//		} 
 
-		dim3 threads_per_block(divisions_in_t, divisions_in_dm);
-		dim3 num_blocks(num_blocks_t, num_blocks_dm);
+			dim3 threads_per_block(divisions_in_t, divisions_in_dm);
+			dim3 num_blocks(num_blocks_t, num_blocks_dm);
 
-		cudaDeviceSetSharedMemConfig (cudaSharedMemBankSizeFourByte);
-		cudaFuncSetCacheConfig(shared_dedisperse_kernel, cudaFuncCachePreferShared);
-		shared_dedisperse_kernel<<<num_blocks, threads_per_block>>>(inBin[i], d_input, d_output, (float) ( startdm / ( *tsamp ) ), (float) ( dm_step[i] / ( *tsamp ) ));
+			cudaDeviceSetSharedMemConfig (cudaSharedMemBankSizeFourByte);
+			cudaFuncSetCacheConfig(shared_dedisperse_kernel_16, cudaFuncCachePreferShared);
+			shared_dedisperse_kernel_16<<<num_blocks, threads_per_block>>>(inBin[i], d_input, d_output, (float) ( startdm / ( *tsamp ) ), (float) ( dm_step[i] / ( *tsamp ) ));
+		}
+		else
+		{
+			printf("\nERROR: smem line length is too short.\nRun the auto tuner again!\n");
+			exit(0);
+		}
+
 	}
 	else
 	{
-		printf("\nERROR: smem line length is too short.\nRun the auto tuner again!\n");
-		exit(0);
-	}
+		// FOR KEPLER SMEM....
+		float shift_one = ( SDIVINDM - 1 ) * ( dm_step[i] / ( *tsamp ) );
+		int shifta = (int) floorf(shift_one * dmshifts[nchans - 1]) + ( SDIVINT - 1 ) * 2;
+		int lineshift = shifta + ( ( SNUMREG - 1 ) * 2 * SDIVINT );
+		printf("\n%f", dm_step[i] / ( *tsamp ));
+		printf("\n%f", dmshifts[nchans - 1]);
+		printf("\n%d", shifta);
+		printf("\nlineshift:\t%d", lineshift);
 
-	//}}}
-	//cudaUnbindTexture(inTex);
-	//cudaDestroyTextureObject(tex);
+		// Check to see if the threadblock will load a shared memory line that
+		// is long enough for the algorithm to run without an out of bounds
+		// access...
+		if (( ( SDIVINT - 1 ) + ( ( SDIVINDM - 1 ) * SDIVINT ) - 1 ) > lineshift)
+		{
+
+			printf("\nUsing fast shared memory kernel");
+
+			//{{{ Dedisperse data on the GPU 
+			float startdm = dm_low[i];
+
+			int divisions_in_t = SDIVINT;
+			int divisions_in_dm = SDIVINDM;
+			int num_blocks_t = t_processed / ( divisions_in_t * 2 * SNUMREG );
+			int num_blocks_dm = ndms[i] / divisions_in_dm;
+
+
+			dim3 threads_per_block(divisions_in_t, divisions_in_dm);
+			dim3 num_blocks(num_blocks_t, num_blocks_dm);
+
+			cudaDeviceSetSharedMemConfig (cudaSharedMemBankSizeFourByte);
+			cudaFuncSetCacheConfig(shared_dedisperse_kernel, cudaFuncCachePreferShared);
+			shared_dedisperse_kernel<<<num_blocks, threads_per_block>>>(inBin[i], d_input, d_output, (float) ( startdm / ( *tsamp ) ), (float) ( dm_step[i] / ( *tsamp ) ));
+		}
+		else
+		{
+			printf("\nERROR: smem line length is too short.\nRun the auto tuner again!\n");
+			exit(0);
+		}
+
+	}
 }
 
 //}}}
