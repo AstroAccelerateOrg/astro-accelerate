106af8f54e8a0bb789f5fc6f6949ceca3666de62
KAdamek <karel.adamek@oerc.ox.ac.uk>
2017-06-16 15:19:21 +0100

    Added switch for base line noise and removed some debug info

--- lib/device_MSD_BLN_pw_kernel.cu
+++ lib/device_MSD_BLN_pw_kernel.cu
@@ -148,51 +148,6 @@ __global__ void MSD_BLN_pw_no_rejection(float const* __restrict__ d_input, float
 	}
 }
 
-/*
-__global__ void MSD_BLN_pw_rejection(float const* __restrict__ d_input, float *d_output, int y_steps, int nTimesamples, int offset, float multiplier, float signal_mean, float signal_sd) {
-	__shared__ float s_input[3*MSD_PW_NTHREADS];
-	float M, S, j, ftemp;
-	
-	int spos = blockIdx.x*MSD_BLN_PW_EPT*MSD_PW_NTHREADS + threadIdx.x;
-	int gpos = blockIdx.y*y_steps*nTimesamples + spos;
-	M=0;	S=0;	j=0;
-	if( spos<(nTimesamples-offset) ){
-		
-		for (int yf = 0; yf < y_steps; yf++) {
-			ftemp=__ldg(&d_input[gpos]);
-			if( (ftemp > (signal_mean - multiplier*signal_sd)) && (ftemp < (signal_mean + multiplier*signal_sd)) ){
-				if(j==0){
-					Initiate( &M, &S, &j, ftemp);
-				}
-				else{
-					Add_one( &M, &S, &j, ftemp);
-				}			
-			}
-			gpos = gpos + nTimesamples;
-		}
-		
-	}
-	
-	s_input[threadIdx.x] = M;
-	s_input[blockDim.x + threadIdx.x] = S;
-	s_input[2*blockDim.x + threadIdx.x] = j;
-	
-	__syncthreads();
-	
-	Reduce_SM( &M, &S, &j, s_input );
-	Reduce_WARP( &M, &S, &j);
-	
-	//----------------------------------------------
-	//---- Writing data
-	if (threadIdx.x == 0) {
-		gpos = blockIdx.y*gridDim.x + blockIdx.x;
-		d_output[3*gpos] = M;
-		d_output[3*gpos + 1] = S;
-		d_output[3*gpos + 2] = j;
-	}
-}
-*/
-
 
 __global__ void MSD_BLN_pw_rejection_normal(float const* __restrict__ d_input, float *d_output, float *d_MSD, int y_steps, int nTimesamples, int offset, float bln_sigma_constant) {
 	__shared__ float s_input[3*MSD_PW_NTHREADS];
@@ -238,7 +193,6 @@ __global__ void MSD_BLN_pw_rejection_normal(float const* __restrict__ d_input, f
 		d_output[3*gpos + 2] = j;
 	}
 }
-
 
 
 __global__ void MSD_GPU_LA_ALL_no_rejection(float const* __restrict__ d_input, float *d_output, float *d_output_taps, int y_steps, int nTaps, int nTimesamples, int offset) {
@@ -526,12 +480,10 @@ __global__ void MSD_GPU_LA_ALL_Nth_no_rejection(float const* __restrict__ d_inpu
 		d_output_taps[3*gpos] = M;
 		d_output_taps[3*gpos + 1] = S;
 		d_output_taps[3*gpos + 2] = j;
-		if(S!=S) printf("M=%f; S=%f; j=%f\n", M, S, j);
 		
 		d_output[3*gpos] = M_b;
 		d_output[3*gpos + 1] = S_b;
 		d_output[3*gpos + 2] = j_b;
-		if(S_b!=S_b) printf("M_b=%f; S_b=%f; j_b=%f\n", M_b, S_b, j_b);
 	}
 }
 
@@ -707,12 +659,10 @@ __global__ void MSD_GPU_LA_ALL_pw_rejection(float const* __restrict__ d_input, f
 		d_output_taps[3*gpos] = M;
 		d_output_taps[3*gpos + 1] = S;
 		d_output_taps[3*gpos + 2] = j;
-		if(S!=S) printf("pw_rejection final: M:%f; S:%f; j:%f\n", M, S, j);
 		
 		d_output[3*gpos] = M_b;
 		d_output[3*gpos + 1] = S_b;
 		d_output[3*gpos + 2] = j_b;
-		if(S_b!=S_b) printf("pw_rejection final: M_b:%f; S_b:%f; j_b:%f\n", M_b, S_b, j_b);
 	}
 }
 
@@ -889,145 +839,13 @@ __global__ void MSD_GPU_LA_ALL_Nth_pw_rejection(float const* __restrict__ d_inpu
 		d_output_taps[3*gpos] = M;
 		d_output_taps[3*gpos + 1] = S;
 		d_output_taps[3*gpos + 2] = j;
-		//if(S!=S) printf("pw_rejection final: M:%f; S:%f; j:%f\n", M, S, j);
 		
 		d_output[3*gpos] = M_b;
 		d_output[3*gpos + 1] = S_b;
 		d_output[3*gpos + 2] = j_b;
-		//if(S_b!=S_b) printf("pw_rejection final: M_b:%f; S_b:%f; j_b:%f\n", M_b, S_b, j_b);
 	}
 }
 
-/*
-__global__ void MSD_GPU_limited_final_create_linear_approx_regular(float *d_input, float *d_output, float *d_MSD_base, int nTaps, int size) {
-	__shared__ float s_input[3*WARP*WARP];
-
-	int pos;
-	float M;
-	float S;
-	float j;
-
-	//----------------------------------------------
-	//---- Calculating of streaming mean and sum of squares
-	pos = threadIdx.x;
-	if (size > blockDim.x) {
-		M = d_input[3*pos];
-		S = d_input[3*pos + 1];
-		j = d_input[3*pos + 2];
-		
-		pos = pos + blockDim.x;
-		while (pos < size) {
-			Merge( &M, &S, &j, d_input[3*pos], d_input[3*pos + 1], d_input[3*pos + 2]);
-			pos = pos + blockDim.x;
-		}
-
-		s_input[threadIdx.x] = M;
-		s_input[blockDim.x + threadIdx.x] = S;
-		s_input[2*blockDim.x + threadIdx.x] = j;
-		
-		__syncthreads();
-
-		// now all threads had saved their work, reduction follows
-		Reduce_SM_regular( &M, &S, &j, s_input);
-
-		// by now we should have only 32 partial results. shuffle reduction follows
-		Reduce_WARP_regular(&M, &S, &j);
-	}
-	else {
-		if (threadIdx.x == 0) {
-			pos = 0;
-			M = d_input[3*pos];
-			S = d_input[3*pos + 1];
-			j = d_input[3*pos + 2];
-			
-			for (pos = 1; pos < size; pos++) {
-				Merge( &M, &S, &j, d_input[3*pos], d_input[3*pos + 1], d_input[3*pos + 2]);
-			}
-		}
-	}
-
-	//----------------------------------------------
-	//---- Writing data
-	if (threadIdx.x == 0) {
-		d_output[0] = M / j;
-		d_output[1] = sqrt(S / j);
-		d_output[2] = (sqrt(S / j) - d_MSD_base[1])/( (float) (nTaps-1));
-		printf("MSD_GPU_limited_final_create_linear_approx_regular: mean:%f; sd:%f; j:%f; \n", M / j, sqrt(S / j), (sqrt(S / j) - d_MSD_base[1])/( (float) (nTaps-1)));
-	}
-}
-*/
-
-/*
-__global__ void MSD_GPU_limited_final_create_linear_approx_nonregular(float *d_input, float *d_output, float *d_MSD_base, int nTaps, int size) {
-	__shared__ float s_input[3*WARP*WARP];
-
-	int pos;
-	float M;
-	float S;
-	float j, jv;
-
-	//----------------------------------------------
-	//---- Calculating of streaming mean and sum of squares
-	pos = threadIdx.x;
-	if (size > blockDim.x) {
-		M = 0;	S = 0;	j = 0;
-		while (pos < size) {
-			jv = d_input[3*pos + 2];
-			if( ((int) jv)!=0 ){
-				if( (int) j==0 ){
-					M = d_input[3*pos]; 
-					S = d_input[3*pos + 1];
-					j = jv;
-				}
-				else {
-					Merge( &M, &S, &j, d_input[3*pos], d_input[3*pos + 1], jv);
-				}
-			}
-			pos = pos + blockDim.x;
-		}
-
-		s_input[threadIdx.x] = M;
-		s_input[blockDim.x + threadIdx.x] = S;
-		s_input[2*blockDim.x + threadIdx.x] = j;
-		
-		__syncthreads();
-
-		// now all threads had saved their work, reduction follows
-		Reduce_SM( &M, &S, &j, s_input);
-
-		// by now we should have only 32 partial results. shuffle reduction follows
-		Reduce_WARP(&M, &S, &j);
-	}
-	else {
-		if (threadIdx.x == 0) {
-			pos = 0;
-			M = 0;	S = 0;	j = 0;
-			for (pos = 1; pos < size; pos++) {
-				jv = d_input[3*pos + 2];
-				if( ((int) jv)!=0 ){
-					if( (int) j==0 ){
-						M = d_input[3*pos]; 
-						S = d_input[3*pos + 1];
-						j = jv;
-					}
-					else {
-						Merge( &M, &S, &j, d_input[3*pos], d_input[3*pos + 1], jv);
-					}
-				}
-			}
-		}
-	}
-
-	//----------------------------------------------
-	//---- Writing data
-	if (threadIdx.x == 0) {
-		d_output[0] = M / j;
-		d_output[1] = sqrt(S / j);
-		d_output[2] = (sqrt(S / j) - d_MSD_base[1])/( (float) (nTaps-1));
-		printf("MSD_GPU_limited_final_create_linear_approx_nonregular: mean:%f; sd:%f; j:%f; \n", M / j, sqrt(S / j), (sqrt(S / j) - d_MSD_base[1])/( (float) (nTaps-1)));
-	}
-}
-*/
 
 __global__ void MSD_GPU_final_create_LA_nonregular(float *d_input, float *d_output, float *d_MSD_base, int nTaps, int size) {
 	__shared__ float s_input[3*WARP*WARP];
@@ -1045,76 +863,6 @@ __global__ void MSD_GPU_final_create_LA_nonregular(float *d_input, float *d_outp
 	}
 }
 
-/*
-__global__ void MSD_GPU_limited_final_create_linear_approx_nonregular_final(float *d_input, float *d_output, float *d_MSD_base, int nTaps, int size) {
-	__shared__ float s_input[3*WARP*WARP];
-
-	int pos;
-	float M;
-	float S;
-	float j, jv;
-
-	//----------------------------------------------
-	//---- Calculating of streaming mean and sum of squares
-	pos = threadIdx.x;
-	if (size > blockDim.x) {
-		M = 0;	S = 0;	j = 0;
-		while (pos < size) {
-			jv = d_input[3*pos + 2];
-			if( ((int) jv)!=0 ){
-				if( (int) j==0 ){
-					M = d_input[3*pos]; 
-					S = d_input[3*pos + 1];
-					j = jv;
-				}
-				else {
-					Merge( &M, &S, &j, d_input[3*pos], d_input[3*pos + 1], jv);
-				}
-			}
-			pos = pos + blockDim.x;
-		}
-
-		s_input[threadIdx.x] = M;
-		s_input[blockDim.x + threadIdx.x] = S;
-		s_input[2*blockDim.x + threadIdx.x] = j;
-		
-		__syncthreads();
-
-		// now all threads had saved their work, reduction follows
-		Reduce_SM( &M, &S, &j, s_input);
-
-		// by now we should have only 32 partial results. shuffle reduction follows
-		Reduce_WARP(&M, &S, &j);
-	}
-	else {
-		if (threadIdx.x == 0) {
-			pos = 0;
-			M = 0;	S = 0;	j = 0;
-			for (pos = 1; pos < size; pos++) {
-				jv = d_input[3*pos + 2];
-				if( ((int) jv)!=0 ){
-					if( (int) j==0 ){
-						M = d_input[3*pos]; 
-						S = d_input[3*pos + 1];
-						j = jv;
-					}
-					else {
-						Merge( &M, &S, &j, d_input[3*pos], d_input[3*pos + 1], jv);
-					}
-				}
-			}
-		}
-	}
-
-	//----------------------------------------------
-	//---- Writing data
-	if (threadIdx.x == 0) {
-		d_output[0] = d_MSD_base[0];
-		d_output[1] = d_MSD_base[1];
-		d_output[2] = (sqrt(S / j) - d_MSD_base[1])/( (float) (nTaps-1));
-	}
-}
-*/
 
 __global__ void MSD_GPU_final_create_LA_Nth_nonregular(float *d_input, float *d_output, float *d_MSD_base, float *d_MSD_DIT, int nTaps, int size, int DIT_value) {
 	__shared__ float s_input[3*WARP*WARP];
@@ -1133,270 +881,4 @@ __global__ void MSD_GPU_final_create_LA_Nth_nonregular(float *d_input, float *d_
 	}
 }
 
-/*
-__global__ void MSD_GPU_limited_final_create_linear_approx_nonregular_final_Nth(float *d_input, float *d_output, float *d_MSD_base, float *d_MSD_DIT, int nTaps, int size, int DIT_value) {
-	__shared__ float s_input[3*WARP*WARP];
-
-	int pos;
-	float M;
-	float S;
-	float j, jv;
-
-	//----------------------------------------------
-	//---- Calculating of streaming mean and sum of squares
-	pos = threadIdx.x;
-	if (size > blockDim.x) {
-		M = 0;	S = 0;	j = 0;
-		while (pos < size) {
-			jv = d_input[3*pos + 2];
-			if( ((int) jv)!=0 ){
-				if( (int) j==0 ){
-					M = d_input[3*pos]; 
-					S = d_input[3*pos + 1];
-					j = jv;
-				}
-				else {
-					Merge( &M, &S, &j, d_input[3*pos], d_input[3*pos + 1], jv);
-				}
-			}
-			pos = pos + blockDim.x;
-		}
-
-		s_input[threadIdx.x] = M;
-		s_input[blockDim.x + threadIdx.x] = S;
-		s_input[2*blockDim.x + threadIdx.x] = j;
-		
-		__syncthreads();
-
-		// now all threads had saved their work, reduction follows
-		Reduce_SM( &M, &S, &j, s_input);
-
-		// by now we should have only 32 partial results. shuffle reduction follows
-		Reduce_WARP(&M, &S, &j);
-	}
-	else {
-		if (threadIdx.x == 0) {
-			pos = 0;
-			M = 0;	S = 0;	j = 0;
-			for (pos = 1; pos < size; pos++) {
-				jv = d_input[3*pos + 2];
-				if( ((int) jv)!=0 ){
-					if( (int) j==0 ){
-						M = d_input[3*pos]; 
-						S = d_input[3*pos + 1];
-						j = jv;
-					}
-					else {
-						Merge( &M, &S, &j, d_input[3*pos], d_input[3*pos + 1], jv);
-					}
-				}
-			}
-		}
-	}
-
-	//----------------------------------------------
-	//---- Writing data
-	if (threadIdx.x == 0) {
-		d_output[0] = d_MSD_base[0];
-		d_output[1] = d_MSD_base[1];
-		d_output[2] = (sqrt(S / j) - d_MSD_base[1])/( (float) (nTaps));
-		d_output[3] = d_MSD_DIT[0]*DIT_value;//*DIT_value
-		printf("MSD_GPU_limited_final_create_linear_approx_nonregular: mean:%f; sd:%f; j:%f; DIT_mean:%f \n", M / j, sqrt(S / j), (sqrt(S / j) - d_MSD_base[1])/( (float) (nTaps)), d_MSD_DIT[0]*DIT_value);
-	}
-}
-*/
 #endif
-
-/*
-__global__ void MSD_GPU_LA_ALL_pw_rejection(float const* __restrict__ d_input, float *d_output, float *d_output_taps, float *d_MSD_T, float *d_MSD_T_base, float bln_sigma_constant, int y_steps, int nTaps, int nTimesamples, int offset) {
-	// This kernel will calculate only sigma_T I might merge it with sigma_1 if performing well
-	// Shared memory required
-	__shared__ float s_input[3*MSD_PW_NTHREADS];
-	__shared__ float s_base[3*MSD_PW_NTHREADS];
-	
-	// MSD variables
-	float M, S, j;
-	float M_b, S_b, j_b;
-	// FIR variables
-	int d, gpos, spos, local_id, EpT;
-	float2 ftemp1, ftemp2, ftemp3;
-	float Bw[2];
-	float signal_mean = d_MSD_T_base[0];
-	float signal_sd = d_MSD_T_base[1];
-	float signal_mean_taps = d_MSD_T[0];
-	float signal_sd_taps = d_MSD_T[1];
-	
-	EpT = 2*MSD_PW_NTHREADS-nTaps+4;
-
-	// First y coordinate is separated
-	//-------------------> FIR
-	spos = blockIdx.x*(EpT) + 2*threadIdx.x;
-	gpos = blockIdx.y*y_steps*nTimesamples + spos;
-	Bw[0]=0; Bw[1]=0; j=0; j_b=0;
-	M=0; S=0; j=0;
-	M_b=0; S_b=0; j_b=0;
-	if( (spos+4)<(nTimesamples-offset) ){
-		// loading data for FIR filter. Each thread calculates two samples
-		ftemp1.x= __ldg(&d_input[gpos]);	
-		ftemp1.y= __ldg(&d_input[gpos+1]);
-		ftemp2.x= __ldg(&d_input[gpos+2]);
-		ftemp2.y= __ldg(&d_input[gpos+3]);
-		ftemp3.x= __ldg(&d_input[gpos+4]);
-		
-		// Initialization of MSD variables for non-processed StrDev
-		if( (ftemp1.x > (signal_mean - bln_sigma_constant*signal_sd)) && (ftemp1.x < (signal_mean + bln_sigma_constant*signal_sd)) ){
-			if(j_b==0) Initiate( &M_b, &S_b, &j_b, ftemp1.x );
-			else Add_one( &M_b, &S_b, &j_b, ftemp1.x );
-			//if(S_b!=S_b) printf("first th:%d; M_b:%f; S_b:%f; j_b:%f [%f;%f]\n", threadIdx.x, M_b, S_b, j_b, ftemp1.x, 0);
-		}
-		if( (ftemp1.y > (signal_mean - bln_sigma_constant*signal_sd)) && (ftemp1.y < (signal_mean + bln_sigma_constant*signal_sd)) ){
-			if(j_b==0) Initiate( &M_b, &S_b, &j_b, ftemp1.y );
-			else Add_one( &M_b, &S_b, &j_b, ftemp1.y );
-			//if(S_b!=S_b) printf("first th:%d; M_b:%f; S_b:%f; j_b:%f [%f;%f]\n", threadIdx.x, M_b, S_b, j_b, 0, ftemp1.y);
-		}
-		
-		Bw[0]=ftemp1.x + ftemp1.y + ftemp2.x + ftemp2.y;
-		Bw[1]=ftemp1.y + ftemp2.x + ftemp2.y + ftemp3.x;
-		
-	}
-	
-	s_input[2*threadIdx.x] = Bw[0];
-	s_input[2*threadIdx.x+1] = Bw[1];
-	
-	__syncthreads();
-	
-	// Calculating FIT up to nTaps
-	for(d=4; d<nTaps; d=d+4){
-		local_id = threadIdx.x-(d>>1);
-		if(local_id>=0){
-			Bw[0] = Bw[0] + s_input[2*local_id]; Bw[1] = Bw[1] + s_input[2*local_id+1];
-		}
-	}
-	
-	// Note: threads with local_id<0 which have wrong result create sums as well but are removed from final results later
-	//       same is for base values as these would be included twice. First time here and next time in threadblock next to it
-	//       this is due to halo needed for FIR filter
-	if( (Bw[0] > (signal_mean_taps - bln_sigma_constant*signal_sd_taps)) && (Bw[0] < (signal_mean_taps + bln_sigma_constant*signal_sd_taps)) ){
-		if(j==0) Initiate( &M, &S, &j, Bw[0] );
-		else Add_one( &M, &S, &j, Bw[0] );
-	}
-	if( (Bw[1] > (signal_mean_taps - bln_sigma_constant*signal_sd_taps)) && (Bw[1] < (signal_mean_taps + bln_sigma_constant*signal_sd_taps)) ){
-		if(j==0) Initiate( &M, &S, &j, Bw[1] );
-		else Add_one( &M, &S, &j, Bw[1] );
-	}
-
-	
-	
-	// Rest of the iteration in y direction	
-	for (int yf = 1; yf < y_steps; yf++) {
-		__syncthreads();
-		//-------------------> FIR
-		spos = blockIdx.x*(EpT) + 2*threadIdx.x;
-		gpos = blockIdx.y*y_steps*nTimesamples + yf*nTimesamples + spos;
-		Bw[0]=0; Bw[1]=0;
-		if( (spos+4)<(nTimesamples-offset) ){
-			ftemp1.x= __ldg(&d_input[gpos]);	
-			ftemp1.y= __ldg(&d_input[gpos+1]);
-			ftemp2.x= __ldg(&d_input[gpos+2]);
-			ftemp2.y= __ldg(&d_input[gpos+3]);
-			ftemp3.x= __ldg(&d_input[gpos+4]);
-
-			Bw[0]=ftemp1.x + ftemp1.y + ftemp2.x + ftemp2.y;
-			Bw[1]=ftemp1.y + ftemp2.x + ftemp2.y + ftemp3.x;
-			
-			if( (ftemp1.x > (signal_mean - bln_sigma_constant*signal_sd)) && (ftemp1.x < (signal_mean + bln_sigma_constant*signal_sd)) ){
-				if(j_b==0) Initiate( &M_b, &S_b, &j_b, ftemp1.x );
-				else Add_one( &M_b, &S_b, &j_b, ftemp1.x );
-			}
-			if( (ftemp1.y > (signal_mean - bln_sigma_constant*signal_sd)) && (ftemp1.y < (signal_mean + bln_sigma_constant*signal_sd)) ){
-				if(j_b==0) Initiate( &M_b, &S_b, &j_b, ftemp1.y );
-				else Add_one( &M_b, &S_b, &j_b, ftemp1.y );
-			}
-			
-		}
-		
-		s_input[2*threadIdx.x] = Bw[0];
-		s_input[2*threadIdx.x+1] = Bw[1];
-	
-		for(d=4; d<nTaps; d=d+4){
-			__syncthreads();
-			local_id = threadIdx.x-(d>>1);
-			if(local_id>=0){
-				Bw[0] = Bw[0] + s_input[2*local_id]; Bw[1] = Bw[1] + s_input[2*local_id+1];
-			}
-		}
-		
-		if( (Bw[0] > (signal_mean_taps - bln_sigma_constant*signal_sd_taps)) && (Bw[0] < (signal_mean_taps + bln_sigma_constant*signal_sd_taps)) ){
-			if(j==0) Initiate( &M, &S, &j, Bw[0] );
-			else Add_one( &M, &S, &j, Bw[0] );
-		}
-		if( (Bw[1] > (signal_mean_taps - bln_sigma_constant*signal_sd_taps)) && (Bw[1] < (signal_mean_taps + bln_sigma_constant*signal_sd_taps)) ){
-			if(j==0) Initiate( &M, &S, &j, Bw[1] );
-			else Add_one( &M, &S, &j, Bw[1] );
-		}
-	}
-	
-	__syncthreads();
-	
-	s_input[threadIdx.x] = 0;
-	s_input[blockDim.x + threadIdx.x] = 0;
-	s_input[2*blockDim.x + threadIdx.x] = 0;
-	
-	s_base[threadIdx.x] = 0;
-	s_base[blockDim.x + threadIdx.x] = 0;
-	s_base[2*blockDim.x + threadIdx.x] = 0;
-	
-	__syncthreads();
-
-	spos=blockIdx.x*(EpT) + 2*threadIdx.x;	
-	// correct results have threads with 0 <= local_id < (2*MSD_PW_NTHREADS-nTaps); All other threads needs to store j=0  
-	if(local_id>=0) {		
-		// Note: ommited number of samples in the last trailing threadblocks is due to -nTaps which is here. 
-		//       Missing data should be contained in local_id. Thus this code is missing some time sample even it it does not need to. 
-		//       When removed it produces different number of added time samples in j and j_b which is wierd
-		if( spos<(nTimesamples-offset-nTaps) ) { // -nTaps might not be necessary
-			s_input[local_id] = M;
-			s_input[blockDim.x + local_id] = S;
-			s_input[2*blockDim.x + local_id] = j;
-			
-			s_base[local_id] = M_b;
-			s_base[blockDim.x + local_id] = S_b;
-			s_base[2*blockDim.x + local_id] = j_b;
-			if(S_b!=S_b) printf("th:%d; M_b:%f; S_b:%f; j_b:%f\n", threadIdx.x, M_b, S_b, j_b);
-		}
-
-	}
-	__syncthreads();
-	
-	// We have finished loading data and creating partials for MSD and now we can reduce the MSD variables
-	
-	//------------------------------------------------------------------------------------
-	//---------> StrDev of processed input
-	Reduce_SM( &M, &S, &j, s_input );
-
-	// by now we should have only 32 partial results. shuffle reduction follows
-	Reduce_WARP( &M, &S, &j);
-	
-	
-	//------------------------------------------------------------------------------------
-	//---------> StrDev of unprocessed input
-	Reduce_SM( &M_b, &S_b, &j_b, s_base );
-	// by now we should have only 32 partial results. shuffle reduction follows
-	Reduce_WARP( &M_b, &S_b, &j_b);
-
-	//----------------------------------------------
-	//---- Writing data
-	if (threadIdx.x == 0) {
-		gpos = blockIdx.y*gridDim.x + blockIdx.x;
-		d_output_taps[3*gpos] = M;
-		d_output_taps[3*gpos + 1] = S;
-		d_output_taps[3*gpos + 2] = j;
-		if(S!=S) printf("pw_rejection final: M:%f; S:%f; j:%f\n", M, S, j);
-		
-		d_output[3*gpos] = M_b;
-		d_output[3*gpos + 1] = S_b;
-		d_output[3*gpos + 2] = j_b;
-		if(S_b!=S_b) printf("pw_rejection final: M_b:%f; S_b:%f; j_b:%f\n", M_b, S_b, j_b);
-	}
-}
-*/
