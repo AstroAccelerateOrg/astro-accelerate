47770d730eb0144f9554128ba9831ce88f85a3f2
Chris Williams <christopher.williams@physcis.ox.ac.uk>
2017-06-29 10:42:54 +0100

    new: unit tests framework (empty) for DedispersionStrategy tests
    Former-commit-id: 86d4d592891625ad5dbd0fb3bc431c3741f58c54

---
+++ lib/astro-accelerate/test/src/DedispersionStrategyTest.cpp
@@ -0,0 +1,31 @@
+#include "DedispersionStrategyTest.h"
+#include "../../DedispersionStrategy.h"
+
+
+namespace astroaccelerate {
+namespace test {
+
+
+DedispersionStrategyTest::DedispersionStrategyTest()
+    : ::testing::Test()
+{
+}
+
+DedispersionStrategyTest::~DedispersionStrategyTest()
+{
+}
+
+void DedispersionStrategyTest::SetUp()
+{
+}
+
+void DedispersionStrategyTest::TearDown()
+{
+}
+
+TEST_F(DedispersionStrategyTest, test_medium_number_of_samples)
+{
+}
+
+} // namespace test
+} // namespace astroaccelerate
