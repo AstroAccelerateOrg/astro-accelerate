14c110144d956adb1593a6e0fed637a12f90892e
Wes Armour <warmour@lothar.arc.local>
2017-08-18 12:49:17 +0100

    Working 4 elements per thread. But has 4 way bank conflict.
    Former-commit-id: 0741a6cfb379b81e69f47654b70ba874d06a8809

--- scripts/profiling.sh
+++ scripts/profiling.sh
@@ -6,39 +6,27 @@ rm -rf profile_results
 
 mkdir profile_results
 
-for unroll in {16,32,64}
+for unroll in {4,8,16,32}
 do
-	for acc in {6,8,10}
+	for acc in {2,4,6,8}
 	do
-		for divint in {6,8,10}
+		for divint in {8,10,12,14,16,18}
 		do
-			for divindm in {32,40}
+			for divindm in {20,25,32,40,50,60}
 			do
-				threads=$(echo "$divint * $divindm" | bc)
+				threads=$(echo "$((divint * $divindm))")
 				if [ $threads -le $maxThreads ]; then
 					cd ../lib
 					pwd
-					rm AstroAccelerate/params.h
-					echo "#define ACCMAX 350" > ./params.txt
-					echo "#define ACCSTEP 11" >> ./params.txt
+					rm headers/params.h
+					cat headers/header > ./params.txt
 					echo "#define UNROLLS $unroll" >> ./params.txt
 					echo "#define SNUMREG $acc" >> ./params.txt
 					echo "#define SDIVINT $divint" >> ./params.txt
 					echo "#define SDIVINDM $divindm" >> ./params.txt
 					echo "#define SFDIVINDM $divindm.0f" >> ./params.txt
-					echo "#define CARD 1" >> ./params.txt
-					echo "#define NOPSSHIFT 5" >> ./params.txt
-					echo "#define NOPSLOOP 3" >> ./params.txt
-					echo "#define NDATAPERLOOP 1" >> ./params.txt
-					echo "#define BINDIVINT 6" >> ./params.txt
-					echo "#define BINDIVINF 32" >> ./params.txt
-					echo "#define CT 256" >> ./params.txt
-					echo "#define CF 2" >> ./params.txt
-					echo "#define NOPS 4.0" >> ./params.txt
-					echo "#define STATST 128" >> ./params.txt
-					echo "#define STATSLOOP 8" >> ./params.txt
 
-					mv params.txt AstroAccelerate/params.h
+					mv params.txt headers/params.h
 	
 					make clean
 			
@@ -46,8 +34,7 @@ do
 
 					cd ../scripts/
 
-					./astro-accelerate.sh ../input_files/one_bit.txt > profile_results/u"$unroll"_a"$acc"_t"$divint"_dm"$divindm"_r"$regcount".dat
-					cp ../lib/AstroAccelerate/params.h profile_results/u"$unroll"_a"$acc"_t"$divint"_dm"$divindm"_r"$regcount".h
+					./astro-accelerate.sh ../input_files/ska_tune.txt > profile_results/u"$unroll"_a"$acc"_t"$divint"_dm"$divindm"_r"$regcount".dat
 			
 					echo "unrolls: $unroll	acc: $acc    divint: $divint    divindm: $divindm    reg: $regcount"
 			
@@ -56,14 +43,3 @@ do
 		done
 	done
 done
-
-optimum=$(grep "Real" * | awk -F" " '{print $4" "$1}' | sort -n | tail -1 | awk -F" " '{print $2}' | awk -F"." '{print $1".h"}')
-
-cp profile_results/$optimum ../lib/AstroAccelerate/params.h
-cd ../lib
-pwd
-make clean
-regcount=$(make -j 16 2>&1 | grep -A2 shared_dedisperse_kernel | tail -1 | awk -F" " '{print $5}')
-cd ../scripts/
-
-
