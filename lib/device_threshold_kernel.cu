4b60f39f2bb77512202af9eb3f2f1ee7cf7669e8
KAdamek <karel.adamek@oerc.ox.ac.uk>
2017-10-02 23:45:20 +0100

    Added thresholding to periodicity search
    Thresholding for periodicity search is enabled by setting "threshold"
    flag in the input file
    
    
    Former-commit-id: 2de10ed83022b8647e83e28a08ddf9a9e9969b31

--- lib/device_threshold_kernel.cu
+++ lib/device_threshold_kernel.cu
@@ -40,4 +40,44 @@ __global__ void THR_GPU_WARP(float const* __restrict__ d_input, ushort *d_input_
 		pos_x = pos_x + WARP;
 	}
 }
+
+
+__global__ void GPU_Threshold_for_periodicity_kernel(float const* __restrict__ d_input, ushort *d_input_harms, float *d_output_list, int *gmem_pos, float threshold, int primary_size, int secondary_size, int DM_shift, int max_list_size, int DIT_value) {
+	int pos_p, pos_s, pos, list_pos, mask, leader;
+	float R;
+	
+	pos_p = blockIdx.x*blockDim.x*THR_ELEM_PER_THREAD + threadIdx.x;
+	pos_s = blockIdx.y*blockDim.y + threadIdx.y;
+	
+	
+	for(int i=0; i<THR_ELEM_PER_THREAD; i++){
+		if( (pos_p<primary_size) && (pos_s<secondary_size)){
+			pos = pos_s*primary_size + pos_p;
+			
+			//--------> Thresholding
+			R=__ldg(&d_input[pos]);
+			if(R > threshold) {
+				mask=__ballot(1);
+				leader=__ffs(mask)-1;
+				if(threadIdx.x==leader) list_pos=atomicAdd(gmem_pos,__popc(mask));
+				list_pos=__shfl(list_pos,leader);
+				list_pos=list_pos+__popc(mask&((1<<threadIdx.x)-1));
+				if(list_pos<max_list_size){
+					d_output_list[4*list_pos]   = pos_p + DM_shift;
+					d_output_list[4*list_pos+1] = pos_s*DIT_value;
+					d_output_list[4*list_pos+2] = R;
+					d_output_list[4*list_pos+3] = (float) d_input_harms[pos];
+				}
+			}
+			//-------------------------<
+			
+		}
+		pos_p = pos_p + blockDim.x;
+	}
+}
+
+
+
+
+
 #endif
