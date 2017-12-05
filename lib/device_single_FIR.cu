eb82ad15abb5e56e12de1e9b89c45fbb45b5c85a
KAdamek <karel.adamek@oerc.ox.ac.uk>
2017-12-05 16:46:02 +0000

    Alternative way of computing StDev

--- lib/device_single_FIR.cu
+++ lib/device_single_FIR.cu
@@ -31,3 +31,33 @@ int PD_FIR(float *d_input, float *d_output, int nTaps, int nDMs, int nTimesample
 	ut = nTimesamples - nCUDAblocks_x * PD_FIR_ACTIVE_WARPS * WARP * PD_FIR_NWINDOWS;
 	return ( ut );
 }
+
+
+int GPU_FIRv1_wrapper(float *d_input, float *d_output, int nTaps, unsigned int nDMs, unsigned int nTimesamples){
+	//---------> Task specific
+	int ut; //unused timesamples
+	int itemp=(int) ((nTaps - 1)/(WARP*PD_FIR_ACTIVE_WARPS)) + 1;
+	int nLoops=PD_FIR_NWINDOWS + itemp;
+	
+	//---------> CUDA block and CUDA grid parameters
+	int nCUDAblocks_x=(int) ((nTimesamples - nTaps + 1)/(PD_FIR_ACTIVE_WARPS*WARP*PD_FIR_NWINDOWS));
+	int nCUDAblocks_y=nDMs; //Head size
+	int SM_size=(PD_FIR_ACTIVE_WARPS*WARP*PD_FIR_NWINDOWS + nTaps - 1)*4;
+	
+	dim3 gridSize(nCUDAblocks_x, nCUDAblocks_y, 1);			//nCUDAblocks_y goes through spectra
+	dim3 blockSize(PD_FIR_ACTIVE_WARPS*WARP, 1, 1); 		//nCUDAblocks_x goes through channels
+	
+	// ----------------------------------------------->
+	// --------> Measured part (Pulse detection FIR)	
+	
+	//---------> Pulse detection FIR
+	cudaDeviceSetCacheConfig(cudaFuncCachePreferShared);
+	cudaDeviceSetSharedMemConfig(cudaSharedMemBankSizeEightByte);
+	PD_FIR_GPUv1<<<gridSize,blockSize,SM_size>>>(d_input, d_output, nTaps, nLoops, nTimesamples);
+
+	// --------> Measured part (Pulse detection FIR)
+	// ----------------------------------------------->
+	
+	ut=nTimesamples - nCUDAblocks_x*PD_FIR_ACTIVE_WARPS*WARP*PD_FIR_NWINDOWS;
+	return(ut);
+}
--- lib/device_single_FIR_kernel.cu
+++ lib/device_single_FIR_kernel.cu
@@ -52,4 +52,46 @@ __global__ void PD_FIR_GPU(float const* __restrict__ d_input, float *d_output, i
 	}
 }
 
+
+
+__global__ void PD_FIR_GPUv1(float const* __restrict__ d_input, float *d_output, int nTaps, int nLoops, unsigned int nTimesamples) {
+	extern __shared__ float s_input[];
+
+	unsigned int itemp,pos;
+	float sum[PD_FIR_NWINDOWS];
+
+
+	//----------------------------------------------
+	//---- Reading data
+	itemp=PD_FIR_ACTIVE_WARPS*WARP*PD_FIR_NWINDOWS + nTaps - 1; //amount of data needed probably define it in params.h as it is also size of the shared memory
+	for(int i=0; i<nLoops; i++){
+		pos=i*PD_FIR_ACTIVE_WARPS*WARP + threadIdx.x;
+		if(pos<itemp){
+			s_input[pos]=d_input[blockIdx.y*nTimesamples + blockIdx.x*PD_FIR_ACTIVE_WARPS*WARP*PD_FIR_NWINDOWS + pos];
+		}
+	}
+	
+	__syncthreads();
+	
+	
+	//----------------------------------------------
+	//---- Calculating FIR version 1
+	for(int i=0; i<PD_FIR_NWINDOWS; i++){
+		pos=i*PD_FIR_ACTIVE_WARPS*WARP + threadIdx.x;
+		sum[i]=0;
+		for(int t=0; t<nTaps; t++){
+			sum[i]+=s_input[pos + t];
+		}
+	}
+
+	//----------------------------------------------
+	//---- Writing data	
+	for(int i=0; i<PD_FIR_NWINDOWS; i++){
+		pos=i*PD_FIR_ACTIVE_WARPS*WARP + threadIdx.x;
+		d_output[blockIdx.y*nTimesamples + blockIdx.x*PD_FIR_ACTIVE_WARPS*WARP*PD_FIR_NWINDOWS + pos]=sum[i];
+	}
+	// This so far ignores DM_PER_BLOCK!!!
+}
+
+
 #endif
