1f287debe6006b728000c4636c3675f6d577671f
KAdamek <karel.adamek@oerc.ox.ac.uk>
2018-03-27 08:53:29 +0100

    Fix: Potential overflow in number of thread-blocks in periodicity peak-find/thresholding

--- lib/device_threshold.cu
+++ lib/device_threshold.cu
@@ -85,32 +85,60 @@ int Threshold_for_periodicity_old(float *d_input, ushort *d_input_harms, float *
 }
 
 int Threshold_for_periodicity(float *d_input, ushort *d_input_harms, float *d_output_list,  int *gmem_pos, float *d_MSD, float threshold, int primary_size, int secondary_size, int DM_shift, int inBin, int max_list_size) {
+	//---------> Nvidia stuff
+	cudaDeviceProp deviceProp;
+	cudaGetDeviceProperties(&deviceProp, CARD);
+	size_t max_x = deviceProp.maxGridSize[0], max_y = deviceProp.maxGridSize[1];
+	THR_init();
+	
 	//---------> Task specific
-	int nBlocks_p, nBlocks_s;
+	int nBlocks_p, nBlocks_s, nRepeats;
+	size_t sec_per_chunk, sec_tail;
 	
 	dim3 gridSize(1, 1, 1);
-	dim3 blockSize(WARP, WARP/2, 1);
+	dim3 blockSize(WARP, WARP, 1);
 	
 	nBlocks_p = (int) (primary_size/(blockSize.x*THR_ELEM_PER_THREAD));
 	if( (primary_size%(blockSize.x*THR_ELEM_PER_THREAD))!=0 ) nBlocks_p++;
+	if((size_t) nBlocks_p > max_x) {printf("Too many DM trials!\n"); exit(1);}
 	
 	nBlocks_s = (int) (secondary_size/blockSize.y);
 	if( (secondary_size%blockSize.y)!=0 ) nBlocks_s++;
 	
-	gridSize.x = nBlocks_p;
-	gridSize.y = nBlocks_s;
-	gridSize.z = 1;
+	nRepeats = ceil( (float) ((float) nBlocks_s)/((float) max_y));
+	
+	sec_per_chunk = (int) (secondary_size/nRepeats);
+	sec_tail = secondary_size - sec_per_chunk*(nRepeats-1);
+	
+	std::vector<size_t> secondary_size_per_chunk;
+	for(int f=0; f<(nRepeats-1); f++){
+		secondary_size_per_chunk.push_back(sec_per_chunk);
+	}
+	secondary_size_per_chunk.push_back(sec_tail);
 	
 	#ifdef THRESHOLD_DEBUG
 	printf("Primary:%d; Secondary:%d\n", primary_size, secondary_size);
-	printf("gridSize: [%d; %d; %d]\n", gridSize.x, gridSize.y, gridSize.z);
+	printf("gridSize: [%d; %d; %d]\n", nBlocks_p, nBlocks_s, gridSize.z);
 	printf("blockSize: [%d; %d; %d]\n", blockSize.x, blockSize.y, blockSize.z);
+	printf("nRepeats: %d; sec_per_chunk: %zu; sec_tail: %zu;\n", nRepeats, sec_per_chunk, sec_tail);
+	printf("Secondary dimensions per chunk: ");
+	for(size_t f=0; f<secondary_size_per_chunk.size(); f++) printf("%zu ", secondary_size_per_chunk[f]); printf("\n");	
 	#endif
+
+	size_t shift = 0;
+	for(int f=0; f<(int) secondary_size_per_chunk.size(); f++){
+		nBlocks_s = (int) (secondary_size_per_chunk[f]/blockSize.y);
+		if( (secondary_size_per_chunk[f]%blockSize.y)!=0 ) nBlocks_s++;
 	
-	THR_init();
-	GPU_Threshold_for_periodicity_kernel<<<gridSize, blockSize>>>(d_input, d_input_harms, d_output_list, gmem_pos, d_MSD, threshold, primary_size, secondary_size, DM_shift, max_list_size, inBin);
+		gridSize.x = nBlocks_p;
+		gridSize.y = nBlocks_s;
+		gridSize.z = 1;
 	
-	checkCudaErrors(cudaGetLastError());
+		GPU_Threshold_for_periodicity_kernel<<<gridSize, blockSize>>>(&d_input[shift*primary_size], d_input_harms, d_output_list, gmem_pos, d_MSD, threshold, primary_size, secondary_size_per_chunk[f], DM_shift, max_list_size, inBin);
+	
+		checkCudaErrors(cudaGetLastError());
+		shift = shift + secondary_size_per_chunk[f];
+	}
 
 	return (0);
 }
