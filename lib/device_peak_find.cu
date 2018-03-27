5481df6b8b1f6d4ca5ba9d2ea82ca62dd4887f46
KAdamek <karel.adamek@oerc.ox.ac.uk>
2018-03-27 08:53:29 +0100

    Fix: Potential overflow in number of thread-blocks in periodicity peak-find/thresholding

--- lib/device_peak_find.cu
+++ lib/device_peak_find.cu
@@ -1,4 +1,5 @@
 // James Sharpe's peak finding code
+//#define PEAK_FIND_DEBUG
 
 #include "headers/device_BC_plan.h"
 
@@ -45,9 +46,55 @@ void Peak_find_for_periodicity_search_old(float *d_input_SNR, ushort *d_input_ha
 	dilate_peak_find_for_periods_old<<<gridSize, blockDim>>>(d_input_SNR, d_input_harmonics, d_peak_list, nTimesamples, nDMs, 0, threshold, max_peak_size, gmem_peak_pos, d_MSD, DM_shift, inBin);
 }
 
-void Peak_find_for_periodicity_search(float *d_input_SNR, ushort *d_input_harmonics, float *d_peak_list, int nDMs, int nTimesamples, float threshold, int max_peak_size, int *gmem_peak_pos, float *d_MSD, int DM_shift, int inBin){
-	dim3 blockDim(32, 32, 1);
-	dim3 gridSize(1 + ((nTimesamples-1)/blockDim.x), 1 + ((nDMs-1)/blockDim.y), 1);
+void Peak_find_for_periodicity_search(float *d_input_SNR, ushort *d_input_harmonics, float *d_peak_list, int secondary_size, int primary_size, float threshold, int max_peak_size, int *gmem_peak_pos, float *d_MSD, int DM_shift, int inBin){
+	// nDMs = secondary_size
+	// nTimesamples = primary_size
+	//---------> Nvidia stuff
+	// find maximum values for maximum grid sizes in x and y
+	cudaDeviceProp deviceProp;
+	cudaGetDeviceProperties(&deviceProp, CARD);
+	size_t max_x = deviceProp.maxGridSize[0], max_y = deviceProp.maxGridSize[1];
 	
-	dilate_peak_find_for_periods<<<gridSize, blockDim>>>(d_input_SNR, d_input_harmonics, d_peak_list, nTimesamples, nDMs, 0, threshold, max_peak_size, gmem_peak_pos, d_MSD, DM_shift, inBin);
+	//---------> Task specific
+	int nBlocks_p, nBlocks_s, nRepeats;
+	size_t sec_per_chunk, sec_tail;
+	dim3 blockDim(32, 32, 1);
+	
+	// number of thread-blocks required for the task
+	nBlocks_p = 1 + ((primary_size-1)/blockDim.x);
+	if((size_t) nBlocks_p > max_x) {printf("Too many DM trials!\n"); exit(1);}
+	nBlocks_s = 1 + ((secondary_size-1)/blockDim.y);
+	
+	// number of kernel launches required for the task
+	nRepeats = ceil( (float) ((float) nBlocks_s)/((float) max_y));
+	sec_per_chunk = (int) (secondary_size/nRepeats);
+	sec_tail = secondary_size - sec_per_chunk*(nRepeats-1);
+	
+	// creating vector with y dim processed by each kernel launch
+	std::vector<size_t> secondary_size_per_chunk;
+	for(int f=0; f<(nRepeats-1); f++){
+		secondary_size_per_chunk.push_back(sec_per_chunk);
+	}
+	secondary_size_per_chunk.push_back(sec_tail);
+	
+	#ifdef PEAK_FIND_DEBUG
+	printf("Primary:%d; Secondary:%d\n", primary_size, secondary_size);
+	printf("gridSize: [%d; %d; %d]\n", nBlocks_p, nBlocks_s, 1);
+	printf("blockSize: [%d; %d; %d]\n", blockDim.x, blockDim.y, blockDim.z);
+	printf("nRepeats: %d; sec_per_chunk: %zu; sec_tail: %zu;\n", nRepeats, sec_per_chunk, sec_tail);
+	printf("Secondary dimensions per chunk: ");
+	for(size_t f=0; f<secondary_size_per_chunk.size(); f++) printf("%zu ", secondary_size_per_chunk[f]); printf("\n");	
+	#endif
+	
+	// launching GPU kernels
+	size_t shift = 0;
+	for(int f=0; f<(int) secondary_size_per_chunk.size(); f++){
+		nBlocks_s = 1 + ((secondary_size_per_chunk[f]-1)/blockDim.y);
+		dim3 gridSize(nBlocks_p, nBlocks_s, 1);
+	
+		dilate_peak_find_for_periods<<<gridSize, blockDim>>>(&d_input_SNR[shift*primary_size], d_input_harmonics, d_peak_list, primary_size, secondary_size, 0, threshold, max_peak_size, gmem_peak_pos, d_MSD, DM_shift, inBin);
+		
+		shift = shift + secondary_size_per_chunk[f];
+	}
+	
 }
