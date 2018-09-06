fb4a1a23c57459f0abb86d05781324249e0b5e74
Cees Carels <20415301+ccarels@users.noreply.github.com>
2018-09-06 18:49:27 +0100

    Create setup.sh script
    This script sets up the required environment variables.
    The script checks for the existence of the suggested CUDA PATH and LD_LIBRARY_PATH paths.
    If they exist, the script adds them to the PATH and LD_LIBRARY_PATH environment variables.
    If either the PATH or the LD_LIBRARY_PATH environment variable are not yet set, the script will set them.
    The script relies on the hardcoded version number and architecture (32 or 64 bit), which can be used to enforce versioning.
    If the suggested locations cannot be found or set, the user is notified and asked to check their settings.

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+echo "Setting up Astro-Accelerate..."
+
+# CUDA required PATH and LD_LIBRARY_PATH
+AA_CUDA_VERSION=8.0
+AA_ARCHITECTURE="64" #Leave blank for 32-bit, set to 64 for 64-bit
+AA_ADD_PATH=/usr/local/cuda-${AA_CUDA_VERSION}/bin
+AA_ADD_LD_LIBRARY_PATH=/usr/local/cuda-${AA_CUDA_VERSION}/lib${AA_ARCHITECTURE}
+echo "Assuming CUDA version is ${AA_CUDA_VERSION}"
+
+# Check if the proposed environment variable path exists
+if [ ! -d "${AA_ADD_PATH}" ]; then
+    echo "* ERROR: Directory ${AA_ADD_PATH} does not exist. Setup exiting..."
+else
+    # Check if PATH environment variable has been set on this system
+    if [[ -z "${PATH}" ]]; then
+	echo "* NOTICE: PATH variable is not yet set."
+	echo "* NOTICE: PATH will be set to ${AA_ADD_PATH}"
+	export PATH=${AA_ADD_PATH}
+    else
+	# PATH variable already exists"
+	if [ -d "$AA_ADD_PATH" ] && [[ ":$PATH:" != *":$AA_ADD_PATH:"* ]]; then
+	    echo "* NOTICE: Adding CUDA path to PATH environment variable."
+	    export PATH="${PATH:+"$PATH:"}$AA_ADD_PATH"
+	elif [[ $PATH = *"${AA_ADD_PATH}"* ]]; then
+	    echo "* NOTICE: PATH already contains CUDA path."
+	else
+	    echo "* ERROR: Could not add ${AA_ADD_PATH} to PATH environment variable."
+	    echo "* Either the requested path could not be found, or it has already been added to your PATH environment."
+            echo "* Please check the CUDA version and path on your system."
+	fi
+    fi
+fi
+
+# Check if the proposed environment variable path exists
+if [ ! -d "${AA_ADD_LD_LIBRARY_PATH}" ]; then
+    echo "* ERROR: Directory ${AA_ADD_LD_LIBRARY_PATH} does not exist. Setup exiting..."
+else
+    # Check if PATH environment variable has been set on this system
+    if [[ -z "${LD_LIBRARY_PATH}" ]]; then
+        echo "* NOTICE: LD_LIBRARY_PATH variable is not yet set."
+        echo "* NOTICE: LD_LIBRARY_PATH will be set to ${AA_ADD_LD_LIBRARY_PATH}"
+        export LD_LIBRARY_PATH=${AA_ADD_LD_LIBRARY_PATH}
+    else
+       	# LD_LIBRARY_PATH variable already exists"
+        if [ -d "$AA_ADD_LD_LIBRARY_PATH" ] && [[ ":$LD_LIBRARY_PATH:" != *":$AA_ADD_LD_LIBRARY_PATH:"* ]]; then
+            echo "* NOTICE: Adding CUDA library path to LD_LIBRARY_PATH environment variable."
+            export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}$AA_ADD_LD_LIBRARY_PATH"
+        elif [[ $LD_LIBRARY_PATH = *"${AA_ADD_LD_LIBRARY_PATH}"* ]]; then
+            echo "* NOTICE: LD_LIBRARY_PATH already contains CUDA path."
+        else
+            echo "* ERROR: Could not add ${AA_ADD_LD_LIBRARY_PATH} to LD_LIBRARY_PATH environment variable."
+            echo "* Either the requested path could not be found, or it has already been added to your LD_LIBRARY_PATH environment."
+            echo "* Please check the CUDA version and library path on your system."
+        fi
+    fi
+fi
+
+echo "Done."
