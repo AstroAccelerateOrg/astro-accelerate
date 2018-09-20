e93138c40562d0da860116478fadea8d4680cfef
carels <carels@astraios.oerc.ox.ac.uk>
2018-09-20 09:27:51 +0100

    Remove nvcc specific restrict keyword from kernel C-style wrapper function in device_corner_turn_kernel.cu and function prototype in header.

--- include/device_corner_turn_kernel.hpp
+++ include/device_corner_turn_kernel.hpp
@@ -3,6 +3,6 @@
 
 void call_kernel_simple_corner_turn_kernel(dim3 block_size, dim3 grid_size, unsigned short *d_input, float *d_output, int primary_size, int secondary_size);
 void call_kernel_simple_corner_turn_kernel(dim3 block_size, dim3 grid_size, float *d_input, float *d_output, int primary_size, int secondary_size);
-void call_kernel_corner_turn_SM_kernel(dim3 grid_size, dim3 block_size, float const* __restrict__ d_input, float *d_output, int primary_size, int secondary_size);
+void call_kernel_corner_turn_SM_kernel(dim3 grid_size, dim3 block_size, float const* d_input, float *d_output, int primary_size, int secondary_size);
 void call_kernel_swap(dim3 block_size, dim3 grid_size, unsigned short *d_input, float *d_output, int nchans, int nsamp);
 #endif
--- src/device_corner_turn_kernel.cu
+++ src/device_corner_turn_kernel.cu
@@ -70,7 +70,7 @@ void call_kernel_simple_corner_turn_kernel(dim3 block_size, dim3 grid_size, floa
   simple_corner_turn_kernel<<<block_size, grid_size>>>(d_input, d_output, primary_size, secondary_size);
 }
 
-void call_kernel_corner_turn_SM_kernel(dim3 grid_size, dim3 block_size, float const* __restrict__ d_input, float *d_output, int primary_size, int secondary_size) {
+void call_kernel_corner_turn_SM_kernel(dim3 grid_size, dim3 block_size, float const* d_input, float *d_output, int primary_size, int secondary_size) {
   corner_turn_SM_kernel<<<grid_size,block_size>>>(d_input, d_output, primary_size, secondary_size);
 }
 
