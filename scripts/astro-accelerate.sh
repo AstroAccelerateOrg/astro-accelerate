53e5e1b9a8028c7af829dae1d0c30cf9e9c52bc3
carels <carels@astraios.oerc.ox.ac.uk>
2018-09-20 11:24:47 +0100

    Update astro-accelerate.sh script to be compatible with build system.

--- scripts/astro-accelerate.sh
+++ scripts/astro-accelerate.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
 job_name=$(basename "$1" .txt)
-echo "job_name ${job_name}"
 
 # find out where this script is running from
-#scripts_dir=$(dirname "$0");
-#cd $scripts_dir;
-#scripts_dir=$PWD
-#echo "scripts_dir ${scripts_dir}"
+scripts_dir=$(dirname "$0");
+cd $scripts_dir;
+scripts_dir=$PWD
 
 # define where to place output and find out executables
-#project_top=$(dirname "$scripts_dir")
-#output_dir="$project_top/output"
-#job_dir=$output_dir/$job_name
-#build_dir=$project_top
-#echo "project_top ${project_top}"
-#echo "output_dir ${output_dir}"
-#echo "job_dir ${job_dir}"
-#echo "build_dir ${build_dir}"
+project_top=$(dirname "$scripts_dir")
+output_dir="$project_top/output"
+job_dir=$output_dir/$job_name
+build_dir=$project_top
 
 input_file=$(readlink -f $1)
-echo "input_file is ${input_file}"
 
-#echo "Making ${job_dir}"
-#mkdir -p $job_dir
+mkdir -p $job_dir
 
-#cd $job_dir
+cd $job_dir
 
 rm -f analysed* 
 rm -f acc*
@@ -35,8 +27,6 @@ rm -f harmonic*
 rm -f candidate*
 rm -f peak*
 
-build_dir=~/astro-accelerate/
-cd ~/output/
 time $build_dir/astro-accelerate $input_file
 
 cat analysed* > global_analysed_frb.dat
