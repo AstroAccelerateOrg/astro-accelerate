5b452d20e35fd8c29d309a6f3a0b50ad5b83c5c9
jan2nov <jan.novotny@oerc.ox.ac.uk>
2018-10-29 11:22:12 +0000

    optimization of the zero_dm_kernel

--- src/device_zero_dm.cu
+++ src/device_zero_dm.cu
@@ -7,15 +7,16 @@
 
 void zero_dm(unsigned short *d_input, int nchans, int nsamp, int nbits) {
 
-	int divisions_in_t  = CT;
-	int num_blocks_t    = nsamp/divisions_in_t;
+	int threads_for_sum  = 256;
+	int num_blocks_t    = nsamp;
+	int shared_memory = threads_for_sum*4;
 
 	printf("\nCORNER TURN!");
-	printf("\n%d %d", nsamp, nchans);
-	printf("\n%d %d", divisions_in_t, 1);
+	printf("\n%d %d %d", nsamp, nchans, threads_for_sum);
+	printf("\n%d %d", CT, 1);
 	printf("\n%d %d", num_blocks_t, 1);
 
-	dim3 threads_per_block(divisions_in_t, 1);
+	dim3 threads_per_block(threads_for_sum, 1);
 	dim3 num_blocks(num_blocks_t,1);
 
 	clock_t start_t, end_t;
@@ -23,7 +24,7 @@ void zero_dm(unsigned short *d_input, int nchans, int nsamp, int nbits) {
 
 	float normalization_factor = ((pow(2,nbits)-1)/2);
 
-	call_kernel_zero_dm_kernel(num_blocks, threads_per_block, d_input, nchans, nsamp, normalization_factor);
+	call_kernel_zero_dm_kernel(num_blocks, threads_per_block, shared_memory, d_input, nchans, nsamp, normalization_factor);
 	cudaDeviceSynchronize();
 
 	end_t = clock();
