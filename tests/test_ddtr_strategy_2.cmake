fdcf393e8aab426f4d4f2f33157ea482d855255d
cc <cc>
2018-11-07 11:01:04 +0000

    Added semicolon in main.cpp for compilation. Added new test to test filterbank_metadata. Added TEST_NAME variable to test STATUS message in the .cmake file for the test.

--- tests/test_ddtr_strategy_2.cmake
+++ tests/test_ddtr_strategy_2.cmake
@@ -1,5 +1,5 @@
 set(TEST_NAME test_ddtr_strategy_2)
-message(STATUS "INFO: Add test_ddtr_strategy_2")
+message(STATUS "INFO: Add ${TEST_NAME}")
 add_executable(${TEST_NAME} "${CMAKE_CURRENT_LIST_DIR}/${TEST_NAME}")
 set_target_properties(${TEST_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/tests")
 target_sources(${TEST_NAME}
