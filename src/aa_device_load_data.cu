0c276c678d232d8490c852900b0c3f4f085da997
carels <carels@astraios.oerc.ox.ac.uk>
2018-11-13 13:46:42 +0000

    Add comment about file extension of aa_device_load_data.cu

--- src/aa_device_load_data.cu
+++ src/aa_device_load_data.cu
@@ -16,6 +16,10 @@
 
 namespace astroaccelerate {
 
+  /**
+   * If the file extension of this file is *.cpp, then the code will compile but there will be a runtime CUDA error when copying to device memory.
+   */
+  
 void load_data(int i, int *inBin, unsigned short *device_pointer, unsigned short *host_pointer, int t_processed, int maxshift, int nchans, float *dmshifts) {
     if(i == -1) {
         const long int length = ( t_processed + maxshift );
