1f1cc61abc18e203d57f8c5ea4c00cebbcb52951
cc <cc>
2018-11-14 16:39:08 +0000

    Continued refactoring reading from input text file.

--- include/aa_config.hpp
+++ include/aa_config.hpp
@@ -11,6 +11,8 @@
 #include "aa_ddtr_plan.hpp"
 #include "aa_compute.hpp"
 #include "aa_permitted_pipelines.hpp"
+#include "host_help.hpp"
+
 
 namespace astroaccelerate {
 
@@ -18,44 +20,29 @@ struct aa_CLI {
     std::vector<std::string> input;
 };
 
-struct aa_DM {
-    float low;
-    float high;
-};
-
 struct aa_config_flags {
-    float narrow;
-    float wide;
-    float aggression;
-    float nsearch;
-    float power;
-    float sigma_cutoff;
-    float sigma_constant;
-    float max_boxcar_width_in_sec;
-    float periodicity_sigma_cutoff; //Should this be int or float?
-    
-    int multi_file;
-    int output_dmt;
-    int nboots;
-    int ntrial_bins;
-    int navdms;
-    int range;
-    int candidate_algorithm;
-    int nb_selected_dm;
-    int failsafe;
-    int periodicity_nHarmonics;
-    
-    std::vector<int> inBin;
-    std::vector<int> outBin;
-    std::vector<aa_DM> selected_dm;
-    struct User_DM {
-        float low;
-        float high;
-        float step;
-    };
-    std::vector<User_DM> user_dm;
-    std::vector<aa_compute::modules> user_modules;
-    std::vector<aa_compute::debug> user_debug;
+  float narrow;
+  float wide;
+  float aggression;
+  float nsearch;
+  float power;
+  float sigma_cutoff;
+  float sigma_constant;
+  float max_boxcar_width_in_sec;
+  float periodicity_sigma_cutoff; //Should this be int or float?
+  
+  int multi_file;
+  int output_dmt;
+  int nboots;
+  int ntrial_bins;
+  int navdms;
+  int range;
+  int candidate_algorithm;
+  int nb_selected_dm;
+  int failsafe;
+  int periodicity_nHarmonics;
+  
+  std::vector<aa_compute::debug> user_debug;
 };
 
 class aa_config {
@@ -67,24 +54,30 @@ public:
     aa_config(aa_compute::pipeline &user_pipeline) : configure_from_file(false), pipeline(user_pipeline) {
         
     }
-    
-    //This specialisation of the setup method happens only if reading from an input_file
+
     const aa_compute::pipeline setup() {
+      return setup(m_ddtr_plan, fpath);
+    }
+  
+    //This specialisation of the setup method happens only if reading from an input_file
+  const aa_compute::pipeline setup(aa_ddtr_plan &ddtr_plan, std::string &file_path) {
         if(configure_from_file) {
-            if(get_user_input(user_cli)) {
+            if(get_user_input()) {
                 if(aa_permitted_pipelines::is_permitted(pipeline)) {
+		    ddtr_plan = m_ddtr_plan;
+		    file_path = fpath;
                     return pipeline;
                 }
                 else {
-                    //User input was read successfully, but pipeline is not permitted
+		    //User input was read successfully, but pipeline is not permitted
                     const aa_compute::pipeline empty = {aa_compute::modules::empty};
                     return empty;
                 }
             }
             else {
-                //Problem reading input file
-                const aa_compute::pipeline empty = {aa_compute::modules::empty};
-                return empty;
+                   //Problem reading input file
+                   const aa_compute::pipeline empty = {aa_compute::modules::empty};
+                   return empty;
             }
         }
         else {
@@ -106,10 +99,10 @@ protected:
     aa_config_flags flg;  // configuration flags
     aa_compute::pipeline pipeline;
     aa_CLI user_cli;
-    std::vector<aa_ddtr_plan::dm> m_dm_ranges;
+    aa_ddtr_plan m_ddtr_plan;
     
     
-    bool get_user_input(const aa_CLI &cli_input) {
+    bool get_user_input() {
         const size_t argc = user_cli.input.size();
         
         FILE *fp_in = NULL;   // Path of the input file (configuration)
@@ -117,15 +110,13 @@ protected:
         
         char string[100];
         
-        if (argc < 2)
-        {
+        if (argc < 2) {
             fprintf(stderr, "Need input file.\n");
+	    help();
             return false;
         }
-        else if (argc == 2 && strcmp(user_cli.input[1].c_str(), "-help") != 0)
-        {
-            if (( fp_in = fopen(user_cli.input[1].c_str(), "r") ) == NULL)
-            {
+        else if (argc == 2 && strcmp(user_cli.input[1].c_str(), "-help") != 0) {
+	  if (( fp_in = fopen(user_cli.input[1].c_str(), "r") ) == NULL) {
                 fprintf(stderr, "Invalid input file!\n");
                 return false;
             }
@@ -142,12 +133,7 @@ protected:
             }
             rewind(fp_in);
             
-            flg.user_dm.resize(flg.range);
-            flg.outBin.resize(flg.range);
-            flg.inBin.resize(flg.range);
-            
-            
-            // temporary variables to read dm range
+	    // temporary variables to read dm range
             float temp_low  = 0;
             float temp_high = 0;
             float temp_step = 0;
@@ -155,79 +141,54 @@ protected:
             int temp_out_bin= 0;
             
             // read dm range if enabled
-            int i=0;
-            while (!feof(fp_in))
-            {
-                if ( fscanf(fp_in, "%s %f %f %f %d %d\n", string, &temp_low, &temp_high, &temp_step, &temp_in_bin, &temp_out_bin) == 0 )
-                {
+            while (!feof(fp_in)) {
+                if ( fscanf(fp_in, "%s %f %f %f %d %d\n", string, &temp_low, &temp_high, &temp_step, &temp_in_bin, &temp_out_bin) == 0 ) {
                     fprintf(stderr, "failed to read input\n");
                     exit(0);
                 }
-                if (strcmp(string, "range") == 0)
-                {
-                    flg.user_dm[i] = {temp_low, temp_high, temp_step};
-                    flg.inBin[i]  = temp_in_bin;
-                    flg.outBin[i] = temp_out_bin;
-                    ++i;
+                if (strcmp(string, "range") == 0) {
+		  m_ddtr_plan.add_dm(temp_low, temp_high, temp_step, temp_in_bin, temp_out_bin);
                 }
             }
             rewind(fp_in);
-            
-            flg.selected_dm.reserve(flg.nb_selected_dm);
-            aa_DM tmp_selected;
-            while (!feof(fp_in))
-            {
-                if ( fscanf(fp_in, "%s %f %f\n", string, &tmp_selected.low, &tmp_selected.high) == 0 )
-                {
-                    fprintf(stderr, "failed to read input\n");
-                    return false;
-                }
-                if (strcmp(string, "selected_dm") == 0)
-                {
-                    flg.selected_dm.push_back(tmp_selected);
-                }
-            }
-            rewind(fp_in);
-            
-            while (!feof(fp_in))
-            {
-                if ( fscanf(fp_in, "%s", string) == 0 )
-                {
+	    pipeline.insert(aa_compute::modules::dedispersion);//Always add dedispersion to the pipeline
+            while (!feof(fp_in)) {
+	      if ( fscanf(fp_in, "%s", string) == 0) {
                     fprintf(stderr, "failed to read input\n");
                     return false;
                 }
                 if (strcmp(string, "debug") == 0)
-                    flg.user_debug.push_back(aa_compute::debug::debug);
+		  flg.user_debug.push_back(aa_compute::debug::debug);
                 if (strcmp(string, "analysis") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::analysis);
+		  pipeline.insert(aa_compute::modules::analysis);
                 if (strcmp(string, "periodicity") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::periodicity);
+		  pipeline.insert(aa_compute::modules::periodicity);
                 if (strcmp(string, "acceleration") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::acceleration);
+		  pipeline.insert(aa_compute::modules::acceleration);
                 if (strcmp(string, "output_ffdot_plan") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::output_ffdot_plan);
+		  pipeline.insert(aa_compute::modules::output_ffdot_plan);
                 if (strcmp(string, "output_fdas_list") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::output_fdas_list);
+		  pipeline.insert(aa_compute::modules::output_fdas_list);
                 if (strcmp(string, "output_dmt") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::output_dmt);
+		  pipeline.insert(aa_compute::modules::output_dmt);
                 if (strcmp(string, "zero_dm") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::zero_dm);
+		  pipeline.insert(aa_compute::modules::zero_dm);
                 if (strcmp(string, "zero_dm_with_outliers") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::zero_dm_with_outliers);
+		  pipeline.insert(aa_compute::modules::zero_dm_with_outliers);
                 if (strcmp(string, "rfi") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::rfi);
+		  pipeline.insert(aa_compute::modules::rfi);
                 if (strcmp(string, "oldrfi") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::old_rfi);
+		  pipeline.insert(aa_compute::modules::old_rfi);
                 if (strcmp(string, "threshold") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::candidate_algorithm);
+		  pipeline.insert(aa_compute::modules::candidate_algorithm);
                 if (strcmp(string, "baselinenoise") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::sps_baseline_noise);
+		  pipeline.insert(aa_compute::modules::sps_baseline_noise);
                 if (strcmp(string, "fdas_custom_fft") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::fdas_custom_fft);
+		  pipeline.insert(aa_compute::modules::fdas_custom_fft);
                 if (strcmp(string, "fdas_inbin") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::fdas_inbin);
+		  pipeline.insert(aa_compute::modules::fdas_inbin);
                 if (strcmp(string, "fdas_norm") == 0)
-                    flg.user_modules.push_back(aa_compute::modules::fdas_norm);
+		  pipeline.insert(aa_compute::modules::fdas_norm);
                 if (strcmp(string, "multi_file") == 0)
                     flg.multi_file = 1;
                 if (strcmp(string, "analysis_debug") == 0)
@@ -328,9 +289,8 @@ protected:
                         return false;
                     }
                 }
-                if (strcmp(string, "file") == 0)
-                {
-                    // this command expand "~" to "home/username/"
+                if (strcmp(string, "file") == 0) {
+                    // This command expands "~" to "home/username/"
                     wordexp_t expanded_string;
                     
                     if ( fscanf(fp_in, "%s", string) == 0 )
@@ -342,19 +302,26 @@ protected:
                     if (( fp = fopen(expanded_string.we_wordv[0], "rb") ) == NULL)
                     {
                         fprintf(stderr, "Invalid data file!\n");
+			help();
                         return false;
                     }
+		    else {
+		      //File is valid
+		      //Close it again for later re-opening
+		      fpath = expanded_string.we_wordv[0];
+		      fclose(fp);
+		    }
                     wordfree(&expanded_string);
                 }
             }
         }
-        else if (argc == 2 && strcmp(user_cli.input[1].c_str(), "-help") == 0)
-        {
-            //help();
-            return false;
+        else if ((argc == 2 && (strcmp(user_cli.input[1].c_str(), "-help") == 0))
+		 ||
+		 (argc == 2 && strcmp(user_cli.input[1].c_str(), "--help") == 0)) {
+	  help();
+	  return false;
         }
-        else
-        {
+        else {
             fprintf(stderr, "Cannot recognise input, try \"./astro-accelerate -help.\"\n");
             return false;
         }
--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,22 @@ int main(int argc, char *argv[]) {
     cli.input.push_back(argv[i]);
   }
   aa_config configuration(cli);
-  configuration.setup();
+  aa_ddtr_plan ddtr_plan;
+  std::string file_path;
+  aa_compute::pipeline pipeline = configuration.setup(ddtr_plan, file_path);
+
+  std::cout << "File path " << file_path << std::endl; 
+  
+  for(size_t i = 0; i < ddtr_plan.range(); i++) {
+    std::cout << ddtr_plan.user_dm(i).low << " " << ddtr_plan.user_dm(i).high << " " << ddtr_plan.user_dm(i).step << " " << ddtr_plan.user_dm(i).inBin << " " << ddtr_plan.user_dm(i).outBin << std::endl;
+  }
 
 
+  for(auto i : pipeline) {
+    std::cout << module_name(i) << std::endl;
+  }
+  
+  
   
   std::cout << "NOTICE: Finished." << std::endl;
   return 0;
