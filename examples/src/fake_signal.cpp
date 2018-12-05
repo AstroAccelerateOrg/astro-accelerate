6e7a45fc0b4c52643d600595395ae1ce6cfcad07
jan2nov <jan.novotny@oerc.ox.ac.uk>
2018-12-05 17:18:34 +0000

    printing info gpu card

--- examples/src/fake_signal.cpp
+++ examples/src/fake_signal.cpp
@@ -10,6 +10,8 @@
 #include "aa_ddtr_strategy.hpp"
 #include "aa_filterbank_metadata.hpp"
 #include "aa_permitted_pipelines_1.hpp"
+#include "aa_device_info.hpp"
+#include "params.hpp"
 
 using namespace astroaccelerate;
 
@@ -35,6 +37,27 @@ int main() {
   const int signal_max_pos = 0; // position of the peak; usefull for gaussian and inverse gaussian signal
   
   aa_filterbank_metadata metadata(tstart, tsamp, nbits, nsamples, fch1, foff, nchans);
+
+
+  aa_device_info device_info;
+  if(device_info.check_for_devices()) {
+    std::cout << "NOTICE: Checked for devices." << std::endl;
+  }
+  else {
+    std::cout << "ERROR: Could not find any devices." << std::endl;
+  }
+
+  aa_device_info::CARD_ID selected_card = CARD;
+  aa_device_info::aa_card_info selected_card_info;
+  if(device_info.init_card(selected_card, selected_card_info)) {
+    std::cout << "NOTICE: init_card complete." << std::endl;
+  }
+  else {
+    std::cout << "ERROR: init_card incomplete." << std::endl;
+  }
+
+  aa_device_info::aa_card_info m_card_info;
+  aa_device_info::print_card_info(m_card_info);
   
   const size_t free_memory = 2147483648; // Free memory on the GPU in bytes
   bool enable_analysis = false;       // The strategy will be optimised to run just dedispersion
@@ -48,25 +71,24 @@ int main() {
   std::vector<float> dm_shifts;
   dm_shifts = strategy.dmshifts();
 
-
-  std::vector<unsigned short> input_data(nsamples*nchans);
-
-  for(auto &i : input_data) {
-    i = 0.0;
-  }
-  
-  aa_permitted_pipelines_1<aa_compute::module_option::zero_dm, false> runner(strategy, input_data.data());
-  if(runner.setup()) {
-    std::vector<float> out;
-    int chunk_idx = 0;
-    std::vector<int> range_samples;
-    // The user should consume the output vector data
-    // upon each iteration of .next(out), since
-    // the vector memory is re-allocated for the next chunk.
-    while(runner.next(out, chunk_idx, range_samples)) {
-      std::cout << "NOTICE: Pipeline running over next chunk." << std::endl;
-    }
-  }
+//  std::vector<unsigned short> input_data(nsamples*nchans);
+//
+//  for(auto &i : input_data) {
+//    i = 0.0;
+//  }
+//  
+//  aa_permitted_pipelines_1<aa_compute::module_option::zero_dm, false> runner(strategy, input_data.data());
+//  if(runner.setup()) {
+//    std::vector<float> out;
+//    int chunk_idx = 0;
+//    std::vector<int> range_samples;
+//    // The user should consume the output vector data
+//    // upon each iteration of .next(out), since
+//    // the vector memory is re-allocated for the next chunk.
+//    while(runner.next(out, chunk_idx, range_samples)) {
+//      std::cout << "NOTICE: Pipeline running over next chunk." << std::endl;
+//    }
+//  }
   
   std::cout << "NOTICE: Finished." << std::endl;
   return 0;
