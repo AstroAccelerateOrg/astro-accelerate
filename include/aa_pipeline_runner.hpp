f9eb1b4508cb87f1ccc11d03b4057c764b70cc67
carels <>
2018-12-10 12:34:55 +0000

    Pipeline configuration separated from run function.

--- include/aa_pipeline_runner.hpp
+++ include/aa_pipeline_runner.hpp
@@ -1,6 +1,7 @@
 #ifndef ASTRO_ACCELERATE_AA_PIPELINE_RUNNER_HPP
 #define ASTRO_ACCELERATE_AA_PIPELINE_RUNNER_HPP
 
+#include <iostream>
 #include <vector>
 
 namespace astroaccelerate {
@@ -15,15 +16,16 @@ namespace astroaccelerate {
     
     virtual bool next() {
       // If a derived class does not implement this method, this method is used.
+      std::cout << "ERROR:  The selected operation is not supported on this pipeline." << std::endl;
       return false;
     }
     virtual bool next(std::vector<float> &, int &, std::vector<int> &) {
       // If a derived class does not implement this method, this method is used.
-      
+      std::cout << "ERROR:  The selected operation is not supported on this pipeline." << std::endl;
       return false;
     }
   protected:
-
+    
   };
     
 } // namespace astroaccelerate
