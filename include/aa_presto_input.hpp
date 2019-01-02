7c253afb70baa4aae09be353701d35bd65643fc8
Cees Carels <>
2019-01-02 18:02:21 +0000

    Further comments to codebase.

--- include/aa_presto_input.hpp
+++ include/aa_presto_input.hpp
@@ -6,12 +6,18 @@
 namespace astroaccelerate {
 
 /**
- * The aa_presto_input class is used to parse data given to astro-accelerate by Presto.
+ * \class aa_presto_input aa_presto_input.hpp "include/aa_presto_input.hpp"
+ * \brief The aa_presto_input class is used to parse data given to astro-accelerate by Presto.
+ * \details Further implementation details implemented in source file src/aa_presto_input.cpp.
+ * \warnings This class is not yet implemented.
  */
 
 class aa_presto_input : public aa_input {
 public:
+  /** \brief Constructor for aa_presto_input. */
   aa_presto_input();
+
+  /** \brief Destructor for aa_presto_input. */
   ~aa_presto_input();
   
 private:
--- src/aa_presto_input.cpp
+++ src/aa_presto_input.cpp
@@ -2,12 +2,14 @@
 
 namespace astroaccelerate {
 
-aa_presto_input::aa_presto_input() {
+  /** \brief Constructor for aa_presto_input. */
+  aa_presto_input::aa_presto_input() {
 
-}
+  }
 
-aa_presto_input::~aa_presto_input() {
+  /** \brief Destructor for aa_presto_input. */
+  aa_presto_input::~aa_presto_input() {
 
-}
+  }
 
 } //namespace astroaccelerate
