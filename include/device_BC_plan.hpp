323d03b3e81c05d9ee9a648b40841a3c83ecb02c
Cees Carels <>
2019-01-03 09:58:44 +0000

    Further comments to codebase.

--- include/device_BC_plan.hpp
+++ include/device_BC_plan.hpp
@@ -3,19 +3,24 @@
 
 namespace astroaccelerate {
 
-struct PulseDetection_plan {
-	int decimated_timesamples;
-	int dtm;
-	int iteration;
-	int nBoxcars;
-	int nBlocks;
-	int output_shift;
-	int shift;
-	int startTaps;
-	int unprocessed_samples;
-	int total_ut;
-};
+  /**
+   * \struct PulseDetection_plan device_BC_plan.hpp "include/device_BC_plan.hpp"
+   * \brief Data structure for holding pulse detection plan for Box Car (BC) plan.
+   * \details The user should not need to interact with this structure directly.
+   */
 
-}
+  struct PulseDetection_plan {
+    int decimated_timesamples;
+    int dtm;
+    int iteration;
+    int nBoxcars;
+    int nBlocks;
+    int output_shift;
+    int shift;
+    int startTaps;
+    int unprocessed_samples;
+    int total_ut;
+  };
+} //namespace astroaccelerate
 
 #endif
