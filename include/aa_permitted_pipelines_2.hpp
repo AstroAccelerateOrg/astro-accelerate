2677daef1b0dabd06d1e4e686af535f0c1f83c9d
carels <>
2019-01-14 16:59:50 +0000

    Permitted pipeline 2 has the ability to write dedispersed data to disk via a #define

--- include/aa_permitted_pipelines_2.hpp
+++ include/aa_permitted_pipelines_2.hpp
@@ -28,6 +28,8 @@
 
 #include <iostream>
 
+//#define EXPORT_DD_DATA
+
 namespace astroaccelerate {
 
   /**
@@ -287,6 +289,73 @@ namespace astroaccelerate {
       cudaMemcpy(host_pointer, device_pointer, size, cudaMemcpyDeviceToHost);
     }
 
+    void Export_DD_data(int range, float ***dedispersed_data, size_t max_nTimesamples, int const*const ndms, int *inBin, const std::string &base_filename, int *const ranges_to_export, int DMs_per_file) {
+      char final_filename[200];
+	
+      int inner_DM_shift;	
+      float *h_data;
+	
+	
+      for(int r=0; r<range; r++){
+	if(ranges_to_export[r]) {
+	  size_t nTimesamples = max_nTimesamples/inBin[r];
+	  size_t nDMs = ndms[r];
+	  if(DMs_per_file<0) DMs_per_file = (int) nDMs;
+	  size_t export_size = nTimesamples*DMs_per_file;
+			
+	  int nRepeats = nDMs/DMs_per_file;
+	  int nRest = nDMs%DMs_per_file;
+	  std::vector<int> chunk_size;
+	  for(int f=0; f<nRepeats; f++) chunk_size.push_back(DMs_per_file);
+	  if(nRest>0) chunk_size.push_back(nRest);
+	  printf("  Data in range %d will be exported into %d files\n", r, (int) chunk_size.size());
+	  float percent_per_DMtrial = 100.0/(nDMs);
+		
+	  h_data = new float[export_size];
+		
+	  sprintf(final_filename,"%s_%d.txt", base_filename.c_str(), r);
+	  std::ofstream FILEOUT;
+	  FILEOUT.open (final_filename, std::ofstream::out);
+	  FILEOUT << nTimesamples << std::endl;
+	  FILEOUT << nDMs << std::endl;
+	  FILEOUT << DMs_per_file << std::endl;
+	  FILEOUT << (int) chunk_size.size() << std::endl;
+	  FILEOUT.close();	
+		
+	  inner_DM_shift = 0;
+	  for(int i=0; i<(int) chunk_size.size(); i++){
+	    sprintf(final_filename,"%s_%d_%d.dat", base_filename.c_str(), r, i);
+				
+	    for(int d=0; d<chunk_size[i]; d++) {
+	      for(size_t t=0; t<nTimesamples; t++) {
+		h_data[d*nTimesamples + t] = dedispersed_data[r][inner_DM_shift + d][t];
+	      }
+	    }
+				
+	    FILE *fp_out;
+	    if (( fp_out = fopen(final_filename, "wb") ) == NULL) {
+	      fprintf(stderr, "Error opening output file!\n");
+	      exit(0);
+	    }
+	    fwrite(h_data, nTimesamples*chunk_size[i]*sizeof(float), 1, fp_out);
+	    fclose(fp_out);
+				
+	    inner_DM_shift = inner_DM_shift + chunk_size[i];
+	    printf("    Exported: %0.2f%% \n", ((float) inner_DM_shift)*percent_per_DMtrial);
+	  }
+		
+	  delete [] h_data;
+	}
+      }
+    }
+
+    /** \brief Save dedispersed data to disk. (Use as a debug feature only). */
+    inline size_t Calculate_sd_per_file_from_file_size(size_t size_in_MB, size_t primary_dimension, size_t floats_per_elements) {
+      size_t nFloats = (size_in_MB*1024*1024)/4;
+      size_t sd_per_file = (nFloats*floats_per_elements)/primary_dimension;
+      return(sd_per_file);
+    }
+    
     /**
      * \brief Run the pipeline by processing the next time chunk of data.
      * \details Process any flags for dumping output or providing it back to the user.
@@ -302,6 +371,20 @@ namespace astroaccelerate {
         printf("\nAmount of telescope time processed: %f", tstart_local);
         printf("\nNumber of samples processed: %ld", inc);
         printf("\nReal-time speedup factor: %lf\n", ( tstart_local ) / time);
+
+#ifdef EXPORT_DD_DATA
+	size_t DMs_per_file = Calculate_sd_per_file_from_file_size(1000, inc, 1);
+	int *ranges_to_export = new int[range];
+	for(size_t f = 0; f < range; f++) {
+	  ranges_to_export[f] = 1;
+	}
+	LOG(log_level::dev_debug, "Exporting dedispersion data...");
+	LOG(log_level::dev_debug, "  DM per file: " + std::to_string(DMs_per_file));
+
+	const int *ndms = m_ddtr_strategy.ndms_data();
+	Export_DD_data((int)range, m_output_buffer, (size_t)inc, ndms, inBin.data(), "DD_data", ranges_to_export, (int)DMs_per_file);
+	delete ranges_to_export;
+#endif	
 	return false; // In this case, there are no more chunks to process.	
       }
       else if(t == 0) {
