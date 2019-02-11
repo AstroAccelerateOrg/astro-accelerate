2e3cb6f78357626d1b22db644791eef178d12d29
jan2nov <jan.novotny@oerc.ox.ac.uk>
2019-02-11 16:22:03 +0000

    prototype of fake signal update

--- examples/src/fake_signal.cpp
+++ examples/src/fake_signal.cpp
@@ -25,8 +25,7 @@ void fake_generate_data(unsigned short *output, float *scale_factor, int *shifts
 //	std::cout << scale_factor[i] << std::endl;
 	int time_pos = (i - signal_max_pos + signal_start)*nchans;
 	for(int j = 0; j < nchans; j++){
-		output[time_pos + j + nchans*shifts_index[i]] = (MAX_VALUE*scale_factor[i]);
-//		std::cout << output[time_pos + j + nchans*shifts_index[i]] << " ";
+		output[j + nchans*shifts_index[j] + time_pos] = 255; // (MAX_VALUE*scale_factor[i]);
 	}
   }
 }
@@ -52,7 +51,7 @@ void inverse_gaussian(float* output, int length, float sigma, int *max_pos){
 
 int main() {
   aa_ddtr_plan ddtr_plan;
-  ddtr_plan.add_dm(0, 150, 0.1, 1, 1); // Add dm_ranges: dm_low, dm_high, dm_step, inBin, outBin (unused).
+  ddtr_plan.add_dm(85, 95, 0.1, 1, 1); // Add dm_ranges: dm_low, dm_high, dm_step, inBin, outBin (unused).
 
   // Filterbank metadata
   // (Data description from "SIGPROC-v3.7 (Pulsar) Signal Processing Programs")
@@ -62,18 +61,19 @@ int main() {
   const double nbits = 8;
   unsigned int nsamples = 3.0/tsamp; // 3s of data in current tsamp at minimum; must be more than signal_start + maxshift
   const double fch1 = 1550;
-  int nchans = 4096;
+  int nchans = 128;
   const double foff = -total_bandwidth/nchans;
-  std::cout << "Nsample " << nsamples << std::endl;
+  std::cout << "Nsamples " << nsamples << std::endl;
 
   // params needed by the fake signal function
-  const double dm_position = 90.0; // at what dm put the signal
-  const int func_width = 15; // width of the signal in terms of # of samples
+  double dm_position = 90.0; // at what dm put the signal
+  const int func_width = 1; // width of the signal in terms of # of samples
   int signal_start = 1.5/tsamp; // position of the signal in time
   int signal_max_pos = 0; // position of the peak; usefull for gaussian and inverse gaussian signal
   float* scale_factor;
   scale_factor = (float *)malloc(func_width*sizeof(float));
-  
+ 
+  std::cout << "Signal start at: " << signal_start << std::endl; 
   inverse_gaussian(scale_factor,func_width, 0.5, &signal_max_pos); // generate the scaling factors for the signal with sigma 0.5, also getting position of the max scale
 
 //  for (int n = 0; n < func_width; n++)
@@ -117,6 +117,16 @@ int main() {
 	shifts_index[i] = floor(dm_shifts[i]*dm_position/tsamp);
   }
 
+  FILE *fp_out3;
+  if (( fp_out3 = fopen("shift_data.dat", "wb") ) == NULL) {
+    fprintf(stderr, "Error opening output file!\n");
+    exit(0);
+  }
+  for(int i = 0; i < nchans; i++)
+	fprintf(fp_out3,"%d\n", shifts_index[i]);
+  fclose(fp_out3);
+
+
 //  std::cout << "dmshift: " << shifts_index[2047] << std::endl;
 //  std::cout << "max_shift: " << strategy.maxshift() << std::endl;
 
@@ -124,8 +134,16 @@ int main() {
   input_data = (unsigned short *)malloc(sizeof(unsigned short)*nsamples*nchans);
   fake_generate_data(input_data, scale_factor, shifts_index, func_width, signal_start, signal_max_pos, nchans);
 
-//  for (int i = 0; i < nchans*nsamples; i++)
-//      if (input_data[i] != 0) std::cout << i << " " << input_data[i] << " ";
+  FILE *fp_out2;
+  if (( fp_out2 = fopen("fake_data.dat", "wb") ) == NULL) {
+    fprintf(stderr, "Error opening output file!\n");
+    exit(0);
+  }
+
+  for (int i = 0; i < nchans; i++)
+	for (int j = 0; j < nsamples; j++)
+		fprintf(fp_out2,"%d %d %i\n", i, j, input_data[j*nchans + i]);
+  fclose(fp_out2);
 
   FILE *fp_out;
   if (( fp_out = fopen("DD_data.dat", "wb") ) == NULL) {
@@ -135,31 +153,23 @@ int main() {
   	
   aa_permitted_pipelines_1<aa_compute::module_option::empty, false> runner(strategy, input_data);
   if(runner.setup()) {
-    std::vector<float> out;
     int chunk_idx = 0;
-    std::vector<int> range_samples;
-    // The user should consume the output vector data
+    std::vector<std::vector<std::vector<float>>> out;
+// The user should consume the output vector data
     // upon each iteration of .next(out), since
     // the vector memory is re-allocated for the next chunk.
-    while(runner.next(out, chunk_idx, range_samples)) {
+    while(runner.next(out, chunk_idx)) {
       std::cout << "NOTICE: Pipeline running over next chunk." << std::endl;
     }
 
-	std::cout << "Size of out " << out.size() << std::endl;
-    for(int i = 0; i < out.size(); i++)
-	fprintf(fp_out, "%.8lf\n", out.at(i));
+    for(size_t i = 0; i < out.at(0).size(); i++)
+	for (size_t j = 0; j < out.at(0).at(i).size(); j++)
+		fprintf(fp_out, "%zu %zu %.8lf \n", i, j, out.at(0).at(i).at(j));
 	
-//      if (out.at(i) != 0) std::cout << "output " << i << " " << out.at(i) << std::endl;
+  std::cout << "Value is: " << out.at(0).at(50).at(23437) << std::endl;
 
-    /* Alternative way of running dedispersion and dumping
-     * the output to disk.
-     * Enable the following lines and remove the above while loop to try it out.
-     */
-//    bool dump_to_disk = true;
-//    while(runner.next(dump_to_disk)) {
-//    std::cout << "NOTICE: Pipeline running over next chunk." << std::endl;
-//    }
   }
+
 
   free(scale_factor);
   free(shifts_index);
