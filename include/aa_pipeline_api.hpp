d8ec01d32399cda50e1f82e1e05be353a66ee5ee
carels <>
2019-02-13 17:13:19 +0000

    Remove excess space from log message.

--- include/aa_pipeline_api.hpp
+++ include/aa_pipeline_api.hpp
@@ -405,7 +405,7 @@ namespace astroaccelerate {
     bool ready() {
       pipeline_ready = false;
       if(!aa_permitted_pipelines::is_permitted(m_requested_pipeline)) {
-	LOG(log_level::error, "The requested pipeline is not permitted and cannot be made ready. ");
+	LOG(log_level::error, "The requested pipeline is not permitted and cannot be made ready.");
 	return false;
       }
         
