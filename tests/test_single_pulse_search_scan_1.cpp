ddf75014867633becc00082d4c162791f68f0e55
KAdamek <karel.adamek@oerc.ox.ac.uk>
2019-11-11 20:31:34 +0000

    Single pulse unit test finished

--- tests/test_single_pulse_search_scan_1.cpp
+++ tests/test_single_pulse_search_scan_1.cpp
@@ -71,6 +71,40 @@ int CompareData(float *CPU_result, float *GPU_result, float CPU_scale, float GPU
 	return(nErrors);
 }
 
+
+int CompareDataTaps(ushort *CPU_result, ushort *GPU_result, float CPU_scale, float GPU_scale, int CPU_offset, int GPU_offset, int CPU_dim_x, int GPU_dim_x, int dim_y, int nSamples, double *total_error, double *mean_error){
+	double total_error_l = 0, mean_error_l = 0;
+	size_t nErrors = 0;
+	int cislo = 50;
+	float error;
+	printf("DEBUG: CompareData variables: CPU_dim_x=%d; GPU_dim_x=%d; dim_y=%d; nSamples=%d;\n", CPU_dim_x, GPU_dim_x, dim_y, nSamples);
+	
+	for(int y=0; y<dim_y; y++){
+		for(int x=0; x<nSamples; x++){
+			int CPU_pos = y*CPU_dim_x + x + CPU_offset;
+			int GPU_pos = y*GPU_dim_x + x + GPU_offset;
+			float CPU, GPU;
+			CPU = (float) CPU_result[CPU_pos]/CPU_scale;
+			GPU = (float) GPU_result[GPU_pos]/GPU_scale;
+			
+			error = abs(CPU - GPU);
+			total_error_l = total_error_l + error;
+			if( error > 1.2 ){
+				nErrors++;
+				if(cislo<40){
+					printf("Error [%f] CPU=%f; GPU=%f; x=%d; y=%d;\n", error, CPU, GPU, x, y);
+					cislo++;
+				}
+			}
+		}
+	}
+	mean_error_l = total_error_l/(((double) nSamples)*((double) dim_y));
+	(*total_error) = total_error_l;
+	(*mean_error) = mean_error_l;
+	return(nErrors);
+}
+
+
 int CompareData(float *CPU_result, float *GPU_result, size_t dim_x, size_t dim_y);
 
 void Decimate_in_time(float *h_input, float *h_CPU_decimate, int DIT_value, int DIT_factor, size_t nDMs, size_t nTimesamples){
@@ -89,30 +123,78 @@ void Decimate_in_time(float *h_input, float *h_CPU_decimate, int DIT_value, int 
 	}
 }
 
+void Single_pulse_search_offcentral_1st(float *h_input, float *h_MSD_interpolated, float *h_CPU_boxcar_values, float *h_output_SNR, ushort *h_output_taps, int maxTaps, size_t nDMs, size_t nTimesamples){
+	int nTaps;
+	size_t d, s, output_pos;
+	float ftemp, SNR, FIR_value, mean, stdev;
+	
+	// Calculate SNR for unprocessed signal (equivalent to producing boxcar of width 1)
+	for(d=0; d<nDMs; d++){
+		for(s=0; s<(nTimesamples-maxTaps+1); s++){
+			h_output_SNR[d*nTimesamples + s]  = (h_input[d*nTimesamples + s] - h_MSD_interpolated[0])/h_MSD_interpolated[1];
+			h_output_taps[d*nTimesamples + s] = 1;
+		}
+	}
+	
+	// Calculating boxcar filters with widths up to and including max_FIR.
+	size_t decimated_timesamples = nTimesamples;
+	size_t dtm = decimated_timesamples>>1;
+	for(d=0; d<nDMs; d++){
+		for(s=0; s<(decimated_timesamples-maxTaps+1); s++){
+			// loading initial data
+			nTaps = 1;
+			FIR_value = h_input[d*decimated_timesamples + s];
+			ftemp=sqrt((float) nTaps);
+			SNR=FIR_value/ftemp;
+			output_pos = d*nTimesamples + s;
+			if(SNR>h_output_SNR[output_pos]) {
+				h_output_SNR[output_pos]=SNR;
+				h_output_taps[output_pos]=nTaps;
+			}
+			
+			for(int t=1; t<maxTaps; t++){
+				nTaps = t+1;
+				mean  = h_MSD_interpolated[2*t];
+				stdev = h_MSD_interpolated[2*t+1];
+				FIR_value = FIR_value + h_input[d*decimated_timesamples + s + t];
+				
+				SNR = (FIR_value-mean)/stdev;
+				output_pos = d*nTimesamples + s;
+				if(SNR>h_output_SNR[output_pos]) {
+					h_output_SNR[output_pos]  = SNR;
+					h_output_taps[output_pos] = nTaps;
+				}
+			}
+			
+			if( (s%2)==0 ) {
+				h_CPU_boxcar_values[d*dtm + (int) (s/2)] = FIR_value;
+			}
+		}
+	}
+	//at this point we should have maximum SNR for each point in the DM/Timesample plane
+	
+	for(d=0; d<nDMs; d++){
+		for(s=(decimated_timesamples-maxTaps+1); s<(decimated_timesamples); s++){
+			output_pos = d*decimated_timesamples + s;
+			if( (s%2)==0 && output_pos<(nDMs*decimated_timesamples) ) {
+				FIR_value = h_input[d*decimated_timesamples + s];			
+				for(int t=1; t<maxTaps; t++) FIR_value = FIR_value + h_input[d*decimated_timesamples + s + t];
+				h_CPU_boxcar_values[d*dtm + (int) (s/2)] = FIR_value;
+			}
+		}
+	}
+	
+}
 
 
 
-int check_boxcar_results(float *h_input, float *h_GPU_boxcar_values, float *h_GPU_decimated, float *h_GPU_output_SNR, ushort *h_GPU_output_taps, int nBoxcars, size_t nTimesamples, size_t nDMs){
+
+int check_boxcar_results(float *h_input, float *h_MSD_interpolated, float *h_GPU_boxcar_values, float *h_GPU_decimated, float *h_GPU_output_SNR, ushort *h_GPU_output_taps, int nBoxcars, size_t nTimesamples, size_t nDMs){
 	float *h_CPU_boxcar_values, *h_CPU_decimated, *h_CPU_output_SNR;
 	ushort *h_CPU_output_taps;
 	
 	size_t dec_nTimesamples = (nTimesamples>>1);
 	double total_error, mean_error;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	int decimate_pass;
@@ -122,10 +204,32 @@ int check_boxcar_results(float *h_input, float *h_GPU_boxcar_values, float *h_GP
 	printf("DEBUG: Decimation comparison: nErrors=%d; total_error = %e; mean_error = %e;\n", decimate_pass, total_error, mean_error);
 	delete[] h_CPU_decimated;
 	
+	int SNR_pass;
+	int boxcar_value_pass;
+	int taps_pass;
+	h_CPU_boxcar_values = new float[dec_nTimesamples*nDMs];
+	h_CPU_output_SNR    = new float[nTimesamples*nDMs*2];
+	h_CPU_output_taps   = new ushort[nTimesamples*nDMs*2];
+	Single_pulse_search_offcentral_1st(h_input, h_MSD_interpolated, h_CPU_boxcar_values, h_CPU_output_SNR, h_CPU_output_taps, nBoxcars, nDMs, nTimesamples);
+	
+	boxcar_value_pass = CompareData(h_CPU_boxcar_values, h_GPU_boxcar_values, 1.0, 1.0, 0, 0, dec_nTimesamples, dec_nTimesamples, nDMs, dec_nTimesamples-(nBoxcars>>1), &total_error, &mean_error);
+	printf("DEBUG: Boxcar value comparison: nErrors=%d; total_error = %e; mean_error = %e;\n", boxcar_value_pass, total_error, mean_error);
+	
+	SNR_pass = CompareData(h_CPU_output_SNR, h_GPU_output_SNR, 1.0, 1.0, 0, 0, nTimesamples, nTimesamples, nDMs, nTimesamples-nBoxcars, &total_error, &mean_error);
+	printf("DEBUG: Output SNR comparison: nErrors=%d; total_error = %e; mean_error = %e;\n", SNR_pass, total_error, mean_error);	
+
+	taps_pass = CompareDataTaps(h_CPU_output_taps, h_GPU_output_taps, 1.0, 1.0, 0, 0, nTimesamples, nTimesamples, nDMs, nTimesamples-nBoxcars, &total_error, &mean_error);
+	printf("DEBUG: Output taps comparison: nErrors=%d; total_error = %e; mean_error = %e;\n", taps_pass, total_error, mean_error);
+	printf("DEBUG: The number of errors for the boxcar filter length might be higher then 0.\n");
+	if(mean_error<1.0e-4) taps_pass = 0;
 	
 	
+	delete[] h_CPU_boxcar_values;
+	delete[] h_CPU_output_SNR;
+	delete[] h_CPU_output_taps;
 	
-	if(decimate_pass==0) return(1);
+	
+	if(decimate_pass==0 && boxcar_value_pass==0 && SNR_pass==0) return(1);
 	else return(0);
 }
 
@@ -142,12 +246,13 @@ int main(void) {
 	float  *d_MSD_interpolated;
 	
 	// Memory
-	size_t free_mem,total_mem;
+	size_t free_mem,total_mem, max_mem;
+	max_mem = 8l*1024l*1024l*1024l;
 	cudaMemGetInfo(&free_mem,&total_mem);
+	if(free_mem>(max_mem)) free_mem = max_mem; 
 	int nTimesamples = 800000;
 	size_t memory_required_per_DM = (4*nTimesamples + 3*(nTimesamples>>1))*sizeof(float);
 	int nDMs = (free_mem>>1)/memory_required_per_DM;
-	if( nDMs > 2600 ) nDMs = 2600; 
 	printf("DEBUG: nTimesamples: %d; nDMs: %d;\n", nTimesamples, nDMs);
 	
 	// HOST variables
@@ -204,12 +309,13 @@ int main(void) {
 	for(int d=0; d<nDMs; d++){
 		for(int s=0; s<nTimesamples; s++){
 			size_t pos = ((size_t) d)*nTimesamples + ((size_t) s);
-			h_input[pos] = rand()/(float)RAND_MAX;
+			//h_input[pos] = rand()/(float)RAND_MAX;
+			h_input[pos] = ((int) rand())%10;
 		}
 	}
 	for(int f=0; f<nBoxcars; f++) {
 		h_MSD_interpolated[f*2] = 0;
-		h_MSD_interpolated[f*2 + 1] = sqrt(f);
+		h_MSD_interpolated[f*2 + 1] = sqrt(f+1);
 	}
 		
 		
@@ -234,7 +340,7 @@ int main(void) {
 		return(0);
 	}	
 	
-	int pass = check_boxcar_results(h_input, h_boxcar_values, h_decimated, h_output_SNR, h_output_taps, nBoxcars, (size_t) nTimesamples, (size_t) nDMs);
+	int pass = check_boxcar_results(h_input, h_MSD_interpolated, h_boxcar_values, h_decimated, h_output_SNR, h_output_taps, nBoxcars, (size_t) nTimesamples, (size_t) nDMs);
 	if(pass==0) std::cout << "Fail." << std::endl;
 	else std::cout << "Runs." << std::endl;
 
