e54d831b3762343a70746af6b697ae1e0e066ecd
jan2nov <jan.novotny@physics.slu.cz>
2020-06-19 11:55:30 +0000

    transfer the logic of D2H in dedispersion to a function

--- include/aa_device_save_data.hpp
+++ include/aa_device_save_data.hpp
@@ -9,7 +9,7 @@ namespace astroaccelerate {
   void save_data_offset(float *device_pointer, size_t device_offset, float *host_pointer, size_t host_offset, size_t size);
 
   /** \brief Asynchronous save a chunk of data of the provided size from the device to the host at a given offset. */
-  void save_data_offset_stream(float *device_pointer, size_t device_offset, float *host_pointer, size_t host_offset, size_t size, cudaStream_t stream);
+  void save_data_offset_stream(int dm_range, int current_time_chunk, int **t_processed, long int inc, int *inBin, int const*const ndms, float *d_DDTR_output, float ***output_buffer);
 } // namespace astroaccelerate
   
 #endif // ASTRO_ACCELERATE_AA_DEVICE_SAVE_DATA_HPP
