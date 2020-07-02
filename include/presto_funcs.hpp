27b08d9af0964781de5ce3716cd701c17fac7ce9
Karel Adámek <karel.adamek@oerc.ox.ac.uk>
2020-07-02 23:06:20 +0200

    Modified header for PRESTO functions

--- include/presto_funcs.hpp
+++ include/presto_funcs.hpp
@@ -7,13 +7,18 @@
 
 namespace astroaccelerate {
 
-  int presto_z_resp_halfwidth(double z, int accuracy);
-  cufftComplex *presto_gen_r_response(double roffset, int numbetween, int numkern);
-  cufftComplex *presto_gen_z_response( double z, int numkern, int numbetween);
-  void presto_place_complex_kernel(cufftComplex * kernel, int numkernel,
-				   cufftComplex * result, int numresult);
-  void presto_dered_sig(cufftComplex * fft, int numamps);
-  void presto_norm(cufftComplex * fft, int numamps);
+	int presto_r_resp_halfwidth(int accuracy);
+	int presto_z_resp_halfwidth(double z, int accuracy);
+	int presto_w_resp_halfwidth(double z, double w, int accuracy);
+	
+
+	cufftComplex *presto_gen_r_response(double roffset, int numbetween, int numkern);
+	cufftComplex* presto_gen_z_response(double roffset, int numbetween, double z, int numkern);
+	cufftComplex *presto_gen_w_response(double roffset, int numbetween, double z, double w, int numkern);
+	
+	void presto_place_complex_kernel(cufftComplex * kernel, int numkernel, cufftComplex * result, int numresult);
+	void presto_dered_sig(cufftComplex * fft, int numamps);
+	void presto_norm(cufftComplex * fft, int numamps);
 
 } // namespace astroaccelerate
   
