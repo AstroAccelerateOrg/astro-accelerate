5df41ced732ab64980371db545bcea30b14f2a2f
Junhao Zhang <junhao.zhang@eng.ox.ac.uk>
2026-04-24 01:06:13 +0100

    Revert "Improve pulscan integration and GPU robustness"
    This reverts commit 576baaf0dfde47de471dc4df1135743971f1ac82.

--- build-tests/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jay/workspace/aa/astro-accelerate/examples
-# Build directory: /home/jay/workspace/aa/astro-accelerate/build-tests/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
